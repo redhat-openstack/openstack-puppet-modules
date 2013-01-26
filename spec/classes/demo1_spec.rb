bb772c2991aabcae81e44006b18df731e1048a6f
Richard Clamp <richardc@unixbeard.net>
2013-01-26 22:15:18 +0000

    fix the spec test

--- spec/classes/demo1_spec.rb
+++ spec/classes/demo1_spec.rb
@@ -2,5 +2,5 @@ require 'spec_helper'
 
 describe 'demo1' do
     it { should create_notify('demo1') }
-    it { should create_datacat('/tmp/demo').with_template('sheeps') }
+    it { should create_datacat('/tmp/demo1').with_template('sheeps') }
 end
