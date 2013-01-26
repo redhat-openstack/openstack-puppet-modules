b08a4cde4a626d9a82c4a9571769006940187cbf
Richard Clamp <richardc@unixbeard.net>
2013-01-26 22:02:56 +0000

    create initial types

---
+++ lib/puppet/type/datacat.rb
@@ -0,0 +1,7 @@
+Puppet::Type.newtype(:datacat) do
+    newparam(:path, :namevar => true) do
+    end
+
+    newparam(:template) do
+    end
+end
---
+++ lib/puppet/type/datacat_fragment.rb
@@ -0,0 +1,10 @@
+Puppet::Type.newtype(:datacat_fragment) do
+    newparam(:name, :namevar => true) do
+    end
+
+    newparam(:target) do
+    end
+
+    newparam(:data) do
+    end
+end
