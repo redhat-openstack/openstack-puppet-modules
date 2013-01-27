dad3a0d1fb9c58b90c3e21069aa77b53f93fe520
Richard Clamp <richardc@unixbeard.net>
2013-01-27 06:44:50 +0000

    autorequire the datacat_fragment
    We want the datacat_fragments to gather data and update the datacat object
    before the datacat flushes, so autorequire mased on Datacat_fragment.target ==
    Datacat.path

--- lib/puppet/type/datacat.rb
+++ lib/puppet/type/datacat.rb
@@ -4,4 +4,10 @@ Puppet::Type.newtype(:datacat) do
 
     newparam(:template) do
     end
+
+    autorequire(:datacat_fragment) do
+        catalog.resources.find_all { |r|
+            r.is_a?(Puppet::Type.type(:datacat_fragment)) and r[:target] == self[:path]
+        }
+    end
 end
