9342a0c5d3c568b136c94abc5db999fc977b2301
Richard Clamp <richardc@unixbeard.net>
2013-02-04 20:24:49 +0000

    pass all the useful file parameters through

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,9 +1,36 @@
-define datacat($template) {
-  file { $title:
-    content  => "CHANGEME\n",
+define datacat(
+  $template,
+  $backup                  = undef,
+  $checksum                = undef,
+  $force                   = undef,
+  $group                   = undef,
+  $owner                   = undef,
+  $mode                    = undef,
+  $path                    = $title,
+  $replace                 = undef,
+  $selinux_ignore_defaults = undef,
+  $selrange                = undef,
+  $selrole                 = undef,
+  $seltype                 = undef,
+  $seluser                 = undef,
+) {
+  file { $path:
+    backup                  => $backup,
+    checksum                => $checksum,
+    content                 => undef, # This will be set by the datacat_collector
+    force                   => $force,
+    group                   => $group,
+    mode                    => $mode,
+    owner                   => $owner,
+    replace                 => $replace,
+    selinux_ignore_defaults => $selinux_ignore_defaults,
+    selrange                => $selrange,
+    selrole                 => $selrole,
+    seltype                 => $seltype,
+    seluser                 => $seluser,
   }
 
-  datacat_collector { $title:
+  datacat_collector { $path:
     template      => $template,
     template_body => template_body($template),
     loglevel      => debug,
