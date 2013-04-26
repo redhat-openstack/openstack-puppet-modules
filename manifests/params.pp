904d79e603d08de6fdcc5d78a64616ec5ffe9a2c
Tomas Barton <barton.tomas@gmail.com>
2013-04-26 16:55:56 +0200

    reverting puppi usage

--- manifests/params.pp
+++ manifests/params.pp
@@ -1,25 +1,25 @@
 class zookeeper::params {
   
-  $myid        = params_lookup('myid', '1')
-  $datastore   = params_lookup('datastore', '/var/lib/zookeeper')
-  $client_port = params_lookup('client_port', 2181)
-  $snap_count  = params_lookup('snap_count', 10000)
-  $log_dir     = params_lookup('log_dir', '/var/log/zookeeper')
-  $cfg_dir     = params_lookup('cfg_dir', '/etc/zookeeper/conf')
-  $user        = params_lookup('user', 'zookeeper')
-  $group       = params_lookup('group', 'zookeeper')
-  $java_bin    = params_lookup('java_bin', '/usr/bin/java')
-  $java_opts   = params_lookup('java_opts', '')
-  $pid_dir     = params_lookup('pid_dir', '/var/run/zookeeper')
-  $pid_file    = params_lookup('pid_file', '$PIDDIR/zookeeper.pid')
-  $zoo_main    = params_lookup('zoo_main', 'org.apache.zookeeper.server.quorum.QuorumPeerMain')
-  $lo4j_prop   = params_lookup('log4j_prop', 'INFO,ROLLINGFILE')
+  $myid        = hiera('myid', '1')
+  $datastore   = hiera('datastore', '/var/lib/zookeeper')
+  $client_port = hiera('client_port', 2181)
+  $snap_count  = hiera('snap_count', 10000)
+  $log_dir     = hiera('log_dir', '/var/log/zookeeper')
+  $cfg_dir     = hiera('cfg_dir', '/etc/zookeeper/conf')
+  $user        = hiera('user', 'zookeeper')
+  $group       = hiera('group', 'zookeeper')
+  $java_bin    = hiera('java_bin', '/usr/bin/java')
+  $java_opts   = hiera('java_opts', '')
+  $pid_dir     = hiera('pid_dir', '/var/run/zookeeper')
+  $pid_file    = hiera('pid_file', '$PIDDIR/zookeeper.pid')
+  $zoo_main    = hiera('zoo_main', 'org.apache.zookeeper.server.quorum.QuorumPeerMain')
+  $lo4j_prop   = hiera('log4j_prop', 'INFO,ROLLINGFILE')
 
-  $servers     = params_lookup('zookeeper_servers', [''], local, array)
+  $servers     = hiera_array('zookeeper_servers', [''])
 
   # log4j properties
-  $rollingfile_threshold = params_lookup('rollingfile_threshold', 'ERROR')
-  $tracfile_threshold    = params_lookup('tracefile_threshold', 'TRACE')
+  $rollingfile_threshold = hiera('rollingfile_threshold', 'ERROR')
+  $tracfile_threshold    = hiera('tracefile_threshold', 'TRACE')
 
 
   
