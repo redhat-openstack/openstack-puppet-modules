287ec96c6cff2965d9b459c345ccce1cfe4ce55d
Tomas Barton <barton.tomas@gmail.com>
2013-04-26 16:28:04 +0200

    config moved to a separate file

---
+++ manifests/config.pp
@@ -0,0 +1,61 @@
+# Class: zookeeper::config
+#
+# This module manages the zookeeper configuration directories
+#
+# Parameters: None
+#
+# Actions: None
+#
+# Requires: zookeeper::install, zookeeper
+#
+# Sample Usage: include zookeeper::config
+#
+class zookeeper::config {
+  require zookeeper::install
+  include zookeeper::params
+
+  file { "${log_dir}":
+    owner => $user,
+    group => $group,
+    mode => 644,
+    ensure => directory,
+  }
+
+  file { "${datastore}":
+    ensure => directory, 
+    owner => $user,
+    group => $group,
+    mode => 644, 
+  }
+
+  file { "${cfg_dir}/myid":
+    ensure => file, 
+    content => template("zookeeper/conf/myid.erb"), 
+    owner => $user,
+    group => $group,
+    mode => 644, 
+  }
+
+  file { "${cfg_dir}/zoo.cfg":
+    owner => $user,
+    group => $group,
+    mode => 644,
+    content => template("zookeeper/conf/zoo.cfg.erb"), 
+  }
+
+  file { "${cfg_dir}/environment":
+    owner => $user,
+    group => $group,
+    mode => 644,
+    content => template("zookeeper/conf/environment.erb"), 
+  }
+
+  file { "${cfg_dir}/log4j.properties":
+    owner => $user,
+    group => $group,
+    mode => 644,
+    content => template("zookeeper/conf/log4j.properties.erb"), 
+  }
+
+
+}
--- manifests/init.pp
+++ manifests/init.pp
@@ -14,55 +14,9 @@
 #   class { 'zookeeper': }
 #
 class zookeeper {
-  require zookeeper::install
-  include zookeeper::params
+  include zookeeper::install
+  include zookeeper::config
   
-  file { "${log_dir}":
-    owner => $user,
-    group => $group,
-    mode => 644,
-    ensure => directory,
-  }
-
-  file { "${datastore}":
-    ensure => directory, 
-    owner => $user,
-    group => $group,
-    mode => 644, 
-    backup => false,
-  }
-
-  file { "${cfg_dir}/myid":
-    ensure => file, 
-    content => template("zookeeper/conf/myid.erb"), 
-    owner => $user,
-    group => $group,
-    mode => 644, 
-    backup => false,
-    require => File["${datastore}"],
-  }
-
-  file { "${cfg_dir}/zoo.cfg":
-    owner => $user,
-    group => $group,
-    mode => 644,
-    content => template("zookeeper/conf/zoo.cfg.erb"), 
-  }
-
-  file { "${cfg_dir}/environment":
-    owner => $user,
-    group => $group,
-    mode => 644,
-    content => template("zookeeper/conf/environment.erb"), 
-  }
-
-  file { "${cfg_dir}/log4j.properties":
-    owner => $user,
-    group => $group,
-    mode => 644,
-    content => template("zookeeper/conf/log4j.properties.erb"), 
-  }
-
   service { "zookeeper":
     ensure  => "running",
     enable  => "true",
