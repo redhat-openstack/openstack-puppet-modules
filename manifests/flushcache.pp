32da795fff375cb52f8092b195b80d01351859e4
robruma <robruma@gmail.com>
2013-04-28 21:14:01 -0400

    Added support for Red Hat clients without the sss_cache tool

--- manifests/flushcache.pp
+++ manifests/flushcache.pp
@@ -3,7 +3,7 @@ define ipa::flushcache (
 ) {
 
   $flushcmd = $::osfamily ? {
-    'RedHat' => "/usr/sbin/sss_cache -UGN >/dev/null 2>&1",
+    'RedHat' => "/bin/bash -c \"if [ -x /usr/sbin/sss_cache ]; then /usr/sbin/sss_cache -UGN >/dev/null 2>&1 ; else /usr/bin/find /var/lib/sss/db -type f -exec rm -f '{}' \; ; fi\"",
     'Debian' => "/usr/sbin/nscd -i passwd -i group -i netgroup >/dev/null 2>&1 ; /usr/sbin/sss_cache -UGN >/dev/null 2>&1",
   }
 
