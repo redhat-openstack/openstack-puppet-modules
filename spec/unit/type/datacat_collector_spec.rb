a16f620776e1f5d9a69ce37089159e3827984b88
Richard Clamp <richardc@unixbeard.net>
2013-07-03 15:52:57 +0100

    fix syntax for 1.8.7

--- spec/unit/type/datacat_collector_spec.rb
+++ spec/unit/type/datacat_collector_spec.rb
@@ -7,11 +7,11 @@ describe Puppet::Type.type(:datacat_collector) do
       @file = Puppet::Type.type(:file).new(:path => '/test')
       @catalog.add_resource @file
 
-      @collector = Puppet::Type.type(:datacat_collector).new(
+      @collector = Puppet::Type.type(:datacat_collector).new({
         :title => "/test",
         :template_body => '<%= @data.keys.sort.map { |x| "#{x}=#{@data[x]}" }.join(",") %>',
         :before => @file.to_s,
-      )
+      })
       @catalog.add_resource @collector
     end
 
@@ -21,43 +21,43 @@ describe Puppet::Type.type(:datacat_collector) do
     end
 
     it "should combine one hash" do
-      @catalog.add_resource Puppet::Type.type(:datacat_fragment).new(
+      @catalog.add_resource Puppet::Type.type(:datacat_fragment).new({
         :title => "hash one",
         :target => '/test',
         :data => { "foo" => "one" },
-      )
+      })
 
       @file.expects(:[]=).with(:content, "foo=one")
       @collector.provider.exists?
     end
 
     it "should combine two hashes, disjunct keys" do
-      @catalog.add_resource Puppet::Type.type(:datacat_fragment).new(
+      @catalog.add_resource Puppet::Type.type(:datacat_fragment).new({
         :title => "hash one",
         :target => '/test',
         :data => { "alpha" => "one" },
-      )
-      @catalog.add_resource Puppet::Type.type(:datacat_fragment).new(
+      })
+      @catalog.add_resource Puppet::Type.type(:datacat_fragment).new({
         :title => "hash two",
         :target => '/test',
         :data => { "bravo" => "two" },
-      )
+      })
 
       @file.expects(:[]=).with(:content, "alpha=one,bravo=two")
       @collector.provider.exists?
     end
 
     it "should combine two hashes, overlapping keys" do
-      @catalog.add_resource Puppet::Type.type(:datacat_fragment).new(
+      @catalog.add_resource Puppet::Type.type(:datacat_fragment).new({
         :title => "hash one",
         :target => '/test',
         :data => { "alpha" => "one" },
-      )
-      @catalog.add_resource Puppet::Type.type(:datacat_fragment).new(
+      })
+      @catalog.add_resource Puppet::Type.type(:datacat_fragment).new({
         :title => "hash two",
         :target => '/test',
         :data => { "alpha" => "two" },
-      )
+      })
 
       @file.expects(:[]=).with(:content, "alpha=two")
       @collector.provider.exists?
