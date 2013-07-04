cadc47cb9f1dfd19f31e11fe3978fb3358ad2904
Richard Clamp <richardc@unixbeard.net>
2013-07-04 11:40:46 +0100

    make better use of the rspec-puppet run matcher
    On looking at some function testing with @deanwilson I see that we can make use
    of the run matcher for functions instead which results in more natural spec.

--- spec/functions/template_body_spec.rb
+++ spec/functions/template_body_spec.rb
@@ -1,11 +1,6 @@
 require 'spec_helper'
 
 describe 'template_body' do
-  it "should raise an error when called with a template that cannot be found" do
-    expect { subject.call(['template_body/really_should_never_exist.erb']) }.to raise_error(Puppet::ParseError)
-  end
-
-  it "should return the body of template_body/test1.erb" do
-    subject.call(['template_body/test1.erb']).should == "Goodbye cruel world\n"
-  end
+  it { should run.with_params('template_body/really_should_never_exist.erb').and_raise_error(Puppet::ParseError) }
+  it { should run.with_params('template_body/test1.erb').and_return("Goodbye cruel world\n") }
 end
