4caccc0f9097ca9cb566b89402d457c1cb1a62d8
Richard Clamp <richardc@unixbeard.net>
2013-07-05 17:37:24 +0100

    datacat_collector is public now

--- lib/puppet/type/datacat_collector.rb
+++ lib/puppet/type/datacat_collector.rb
@@ -1,13 +1,44 @@
 Puppet::Type.newtype(:datacat_collector) do
-  desc 'This type is not used directly, it is wrapped by the datacat define'
+  desc """
+  Manages the merginig of data and updating a related resource parameter.
+  
+  The `datacat_collector` type deeply merges a data hash from
+  the `datacat_fragment` resources that target it.
+
+  These fragments are then rendered via an erb template specified by the
+  `template_body` parameter and used to update the `target_field` property
+  of the related `target_resource`.
+
+  Sample usage:
+
+    datacat_collector { 'open_ports':
+      template_body => '<%= @data[\"ports\"].sort.join(\",\") %>',
+      target_resource => File_line['open_ports'],
+      target_field    => 'line',
+    }
+
+    datacat_fragment { 'open webserver':
+      target => 'open_ports',
+      data   => { ports => [ 80, 443 ] },
+    }
+
+    datacat_fragment { 'open ssh':
+      target => 'open_ports',
+      data   => { ports => [ 22 ] },
+    }
+
+
+  For convenience the common use case of targeting a file is wrapped in the
+  datacat defined type.
+  """
   ensurable
 
   newparam(:path, :namevar => true) do
-    desc 'The path of the file we\'re collecting for.'
+    desc "An identifier (typically a file path) that can be used by datacat_fragments so they know where to target the data."
   end
 
   newparam(:target_resource) do
-    desc 'The resource to set data to'
+    desc "The resource that we're going to set the field (eg File['/tmp/demo']) set theto set data tor"
   end
 
   newparam(:target_field) do
