9e9d7c1fe8c07d39c4859e83cf467b6d73cf3dd4
Michael Arnold <github@razorsedge.org>
2013-07-05 23:21:35 -0700

    Add more Puppet tests.

---
+++ tests/snmpd-snmptrapd.pp
@@ -0,0 +1,8 @@
+class { 'snmp':
+  ro_community        => 'SeCrEt',
+  trap_service_ensure => 'running',
+  trap_handlers       => [
+    'traphandle default /usr/bin/perl /usr/bin/traptoemail me@somewhere.com',
+    'traphandle TRAP-TEST-MIB::demo-trap /home/user/traptest.sh demo-trap',
+  ],
+}
---
+++ tests/trapd.pp
@@ -0,0 +1,9 @@
+class { 'snmp':
+  ro_community        => 'SeCrEt',
+  service_ensure      => 'stopped',
+  trap_service_ensure => 'running',
+  trap_handlers       => [
+    'traphandle default /usr/bin/perl /usr/bin/traptoemail me@somewhere.com',
+    'traphandle TRAP-TEST-MIB::demo-trap /home/user/traptest.sh demo-trap',
+  ],
+}
