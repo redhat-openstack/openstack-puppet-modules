c67c5f025c0415e228d5c5e0c1c68c8eca53e3df
Michael Arnold <github@razorsedge.org>
2013-07-07 08:32:53 -0700

    Fix arrow alignment to make lint happy.

--- manifests/params.pp
+++ manifests/params.pp
@@ -17,37 +17,37 @@ class snmp::params {
   # If we have a top scope variable defined, use it, otherwise fall back to a
   # hardcoded value.
   $ro_community = $::snmp_ro_community ? {
-    undef => 'public',
+    undef   => 'public',
     default => $::snmp_ro_community,
   }
 
   $rw_community = $::snmp_rw_community ? {
-    undef => 'private',
+    undef   => undef,
     default => $::snmp_rw_community,
   }
 
   $ro_network = $::snmp_ro_network ? {
-    undef => '127.0.0.1',
+    undef   => '127.0.0.1',
     default => $::snmp_ro_network,
   }
 
   $rw_network = $::snmp_rw_network ? {
-    undef => '127.0.0.1',
+    undef   => '127.0.0.1',
     default => $::snmp_rw_network,
   }
 
   $contact = $::snmp_contact ? {
-    undef => 'Unknown',
+    undef   => 'Unknown',
     default => $::snmp_contact,
   }
 
   $location = $::snmp_location ? {
-    undef => 'Unknown',
+    undef   => 'Unknown',
     default => $::snmp_location,
   }
 
   $views = $::snmp_views ? {
-    undef => [
+    undef   => [
       'view    systemview    included   .1.3.6.1.2.1.1',
       'view    systemview    included   .1.3.6.1.2.1.25.1.1',
     ],
@@ -55,43 +55,43 @@ class snmp::params {
   }
 
   $accesses = $::snmp_accesses ? {
-    undef => [
+    undef   => [
       'access  notConfigGroup ""      any       noauth    exact  systemview none none',
     ],
     default => $::snmp_accesses,
   }
 
   $trap_handlers = $::snmp_trap_handlers ? {
-    undef => [],
+    undef   => [],
     default => $::snmp_trap_handlers,
   }
 
   $snmp_config = $::snmp_snmp_config ? {
-    undef => [],
+    undef   => [],
     default => $::snmp_snmp_config,
   }
 
 ### The following parameters should not need to be changed.
 
   $ensure = $::snmp_ensure ? {
-    undef => 'present',
+    undef   => 'present',
     default => $::snmp_ensure,
   }
 
   $service_ensure = $::snmp_service_ensure ? {
-    undef => 'running',
+    undef   => 'running',
     default => $::snmp_service_ensure,
   }
 
   $trap_service_ensure = $::snmp_trap_service_ensure ? {
-    undef => 'stopped',
+    undef   => 'stopped',
     default => $::snmp_trap_service_ensure,
   }
 
   # Since the top scope variable could be a string (if from an ENC), we might
   # need to convert it to a boolean.
   $autoupgrade = $::snmp_autoupgrade ? {
-    undef => false,
+    undef   => false,
     default => $::snmp_autoupgrade,
   }
   if is_string($autoupgrade) {
@@ -101,7 +101,7 @@ class snmp::params {
   }
 
   $install_client = $::snmp_install_client ? {
-    undef => false,
+    undef   => false,
     default => $::snmp_install_client,
   }
   if is_string($install_client) {
@@ -111,7 +111,7 @@ class snmp::params {
   }
 
   $service_enable = $::snmp_service_enable ? {
-    undef => true,
+    undef   => true,
     default => $::snmp_service_enable,
   }
   if is_string($service_enable) {
@@ -121,7 +121,7 @@ class snmp::params {
   }
 
   $service_hasstatus = $::snmp_service_hasstatus ? {
-    undef => true,
+    undef   => true,
     default => $::snmp_service_hasstatus,
   }
   if is_string($service_hasstatus) {
@@ -131,7 +131,7 @@ class snmp::params {
   }
 
   $service_hasrestart = $::snmp_service_hasrestart ? {
-    undef => true,
+    undef   => true,
     default => $::snmp_service_hasrestart,
   }
   if is_string($service_hasrestart) {
@@ -141,7 +141,7 @@ class snmp::params {
   }
 
   $trap_service_enable = $::snmp_trap_service_enable ? {
-    undef => false,
+    undef   => false,
     default => $::snmp_trap_service_enable,
   }
   if is_string($trap_service_enable) {
@@ -151,7 +151,7 @@ class snmp::params {
   }
 
   $trap_service_hasstatus = $::snmp_trap_service_hasstatus ? {
-    undef => true,
+    undef   => true,
     default => $::snmp_trap_service_hasstatus,
   }
   if is_string($trap_service_hasstatus) {
@@ -161,7 +161,7 @@ class snmp::params {
   }
 
   $trap_service_hasrestart = $::snmp_trap_service_hasrestart ? {
-    undef => true,
+    undef   => true,
     default => $::snmp_trap_service_hasrestart,
   }
   if is_string($trap_service_hasrestart) {
@@ -199,55 +199,55 @@ class snmp::params {
           $snmptrapd_options = '-Lsd -p /var/run/snmptrapd.pid'
         }
       }
-      $package_name        = 'net-snmp'
-      $service_config      = '/etc/snmp/snmpd.conf'
-      $service_config_perms= '0644'
-      $service_name        = 'snmpd'
-      $varnetsnmp_owner    = 'root'
-      $varnetsnmp_group    = 'root'
+      $package_name         = 'net-snmp'
+      $service_config       = '/etc/snmp/snmpd.conf'
+      $service_config_perms = '0644'
+      $service_name         = 'snmpd'
+      $varnetsnmp_owner     = 'root'
+      $varnetsnmp_group     = 'root'
 
-      $client_package_name = 'net-snmp-utils'
-      $client_config       = '/etc/snmp/snmp.conf'
+      $client_package_name  = 'net-snmp-utils'
+      $client_config        = '/etc/snmp/snmp.conf'
 
-      $trap_service_config = '/etc/snmp/snmptrapd.conf'
-      $trap_service_name   = 'snmptrapd'
+      $trap_service_config  = '/etc/snmp/snmptrapd.conf'
+      $trap_service_name    = 'snmptrapd'
     }
     'Debian': {
-      $package_name        = 'snmpd'
-      $service_config      = '/etc/snmp/snmpd.conf'
-      $service_config_perms= '0600'
-      $service_name        = 'snmpd'
-      $snmpd_options       = '-Lsd -Lf /dev/null -u snmp -g snmp -I -smux -p /var/run/snmpd.pid'
-      $sysconfig           = '/etc/default/snmpd'
-      $var_net_snmp        = '/var/lib/snmp'
-      $varnetsnmp_perms    = '0755'
-      $varnetsnmp_owner    = 'snmp'
-      $varnetsnmp_group    = 'snmp'
+      $package_name         = 'snmpd'
+      $service_config       = '/etc/snmp/snmpd.conf'
+      $service_config_perms = '0600'
+      $service_name         = 'snmpd'
+      $snmpd_options        = '-Lsd -Lf /dev/null -u snmp -g snmp -I -smux -p /var/run/snmpd.pid'
+      $sysconfig            = '/etc/default/snmpd'
+      $var_net_snmp         = '/var/lib/snmp'
+      $varnetsnmp_perms     = '0755'
+      $varnetsnmp_owner     = 'snmp'
+      $varnetsnmp_group     = 'snmp'
 
-      $client_package_name = 'snmp'
-      $client_config       = '/etc/snmp/snmp.conf'
+      $client_package_name  = 'snmp'
+      $client_config        = '/etc/snmp/snmp.conf'
 
-      $trap_service_config = '/etc/snmp/snmptrapd.conf'
-      $snmptrapd_options   = '-Lsd -p /var/run/snmptrapd.pid'
+      $trap_service_config  = '/etc/snmp/snmptrapd.conf'
+      $snmptrapd_options    = '-Lsd -p /var/run/snmptrapd.pid'
     }
     'Suse': {
-      $package_name        = 'net-snmp'
-      $service_config      = '/etc/snmp/snmpd.conf'
-      $service_config_perms= '0600'
-      $service_name        = 'snmpd'
-      $snmpd_options       = 'd'
-      $sysconfig           = '/etc/sysconfig/net-snmp'
-      $var_net_snmp        = '/var/lib/net-snmp'
-      $varnetsnmp_perms    = '0755'
-      $varnetsnmp_owner    = 'root'
-      $varnetsnmp_group    = 'root'
+      $package_name         = 'net-snmp'
+      $service_config       = '/etc/snmp/snmpd.conf'
+      $service_config_perms = '0600'
+      $service_name         = 'snmpd'
+      $snmpd_options        = 'd'
+      $sysconfig            = '/etc/sysconfig/net-snmp'
+      $var_net_snmp         = '/var/lib/net-snmp'
+      $varnetsnmp_perms     = '0755'
+      $varnetsnmp_owner     = 'root'
+      $varnetsnmp_group     = 'root'
 
-      $client_package_name = 'net-snmp'
-      $client_config       = '/etc/snmp/snmp.conf'
+      $client_package_name  = 'net-snmp'
+      $client_config        = '/etc/snmp/snmp.conf'
 
-      $trap_service_config = '/etc/snmp/snmptrapd.conf'
-      $trap_service_name   = 'snmptrapd'
-      $snmptrapd_options   = ''
+      $trap_service_config  = '/etc/snmp/snmptrapd.conf'
+      $trap_service_name    = 'snmptrapd'
+      $snmptrapd_options    = ''
     }
     default: {
       fail("Module ${::module} is not supported on ${::operatingsystem}")
