a63e2e1f72af1b2809385cf9b5f5f30ec0382de1
Richard Clamp <richardc@unixbeard.net>
2013-07-08 12:00:59 +0100

    Fix.  We didn't rename the File.  Doh.

--- manifests/init.pp
+++ manifests/init.pp
@@ -60,8 +60,8 @@ define datacat(
   datacat_collector { $title:
     template        => $template_real,
     template_body   => $template_body_real,
-    target_resource => File[$title], # when we evaluate we modify the private data of this resource
+    target_resource => File[$path], # when we evaluate we modify the private data of this resource
     target_field    => 'content',
-    before          => File[$title], # we want to evaluate before that resource so it can do the work
+    before          => File[$path], # we want to evaluate before that resource so it can do the work
   }
 }
