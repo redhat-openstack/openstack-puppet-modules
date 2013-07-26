b5893b4b55cb33ea309dfec4910132a34d9cf62b
robruma <robruma@gmail.com>
2013-07-26 00:20:21 -0400

    Updated flushcache command for Debian hosts

--- manifests/flushcache.pp
+++ manifests/flushcache.pp
@@ -4,7 +4,7 @@ define ipa::flushcache (
 
   $flushcmd = $::osfamily ? {
     'RedHat' => "/bin/bash -c \"if [ -x /usr/sbin/sss_cache ]; then /usr/sbin/sss_cache -UGNA >/dev/null 2>&1 ; else /usr/bin/find /var/lib/sss/db -type f -exec rm -f '{}' \; ; fi\"",
-    'Debian' => "/usr/sbin/nscd -i passwd -i group -i netgroup >/dev/null 2>&1 ; /usr/sbin/sss_cache -UGNA >/dev/null 2>&1"
+    'Debian' => "/bin/bash -c \"if [ -x /usr/sbin/nscd ]; then /usr/sbin/nscd -i passwd -i group -i netgroup -i automount >/dev/null 2>&1 ; elif [ -x /usr/sbin/sss_cache ]; then /usr/sbin/sss_cache -UGNA >/dev/null 2>&1 ; else /usr/bin/find /var/lib/sss/db -type f -exec rm -f '{}' \; ; fi\""
   }
 
   exec { "flushcache-${host}":
