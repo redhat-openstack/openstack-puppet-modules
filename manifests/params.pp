37d9b730f2011b164b2b268470aa2a03e5450d15
Tomas Barton <barton.tomas@gmail.com>
2013-10-09 13:39:25 +0200

    prefixing zookeeper parameters

--- manifests/params.pp
+++ manifests/params.pp
@@ -1,29 +1,29 @@
 class zookeeper::params {
   
-  $myid        = hiera('myid', '1')
-  $datastore   = hiera('datastore', '/var/lib/zookeeper')
-  $client_port = hiera('client_port', 2181)
-  $snap_count  = hiera('snap_count', 10000)
-  $log_dir     = hiera('log_dir', '/var/log/zookeeper')
-  $cfg_dir     = hiera('cfg_dir', '/etc/zookeeper/conf')
-  $user        = hiera('user', 'zookeeper')
-  $group       = hiera('group', 'zookeeper')
-  $java_bin    = hiera('java_bin', '/usr/bin/java')
-  $java_opts   = hiera('java_opts', '')
-  $pid_dir     = hiera('pid_dir', '/var/run/zookeeper')
-  $pid_file    = hiera('pid_file', '$PIDDIR/zookeeper.pid')
-  $zoo_main    = hiera('zoo_main', 'org.apache.zookeeper.server.quorum.QuorumPeerMain')
-  $lo4j_prop   = hiera('log4j_prop', 'INFO,ROLLINGFILE')
+  $myid        = hiera('zookeeper::id', '1')
+  $datastore   = hiera('zookeeper::datastore', '/var/lib/zookeeper')
+  $client_port = hiera('zookeeper::client_port', 2181)
+  $snap_count  = hiera('zookeeper::snap_count', 10000)
+  $log_dir     = hiera('zookeeper::log_dir', '/var/log/zookeeper')
+  $cfg_dir     = hiera('zookeeper::cfg_dir', '/etc/zookeeper/conf')
+  $user        = hiera('zookeeper::user', 'zookeeper')
+  $group       = hiera('zookeeper::group', 'zookeeper')
+  $java_bin    = hiera('zookeeper::java_bin', '/usr/bin/java')
+  $java_opts   = hiera('zookeeper::java_opts', '')
+  $pid_dir     = hiera('zookeeper::pid_dir', '/var/run/zookeeper')
+  $pid_file    = hiera('zookeeper:pid_file', '$PIDDIR/zookeeper.pid')
+  $zoo_main    = hiera('zookeeper::zoo_main', 'org.apache.zookeeper.server.quorum.QuorumPeerMain')
+  $lo4j_prop   = hiera('zookeeper::log4j_prop', 'INFO,ROLLINGFILE')
 
 
-  $servers     = hiera_array('zookeeper_servers', [''])
+  $servers     = hiera_array('zookeeper::servers', [''])
 
-  $snapRetainCount = hiera('zookeeper_snapRetainCount', 3)
+  $snapRetainCount = hiera('zookeeper::snapRetainCount', 3)
   # interval in hours, purging enabled when >= 1
-  $purgeInterval   = hiera('zookeeper_purgeInterval', 0)
+  $purgeInterval   = hiera('zookeeper::purgeInterval', 0)
 
   # log4j properties
-  $rollingfile_threshold = hiera('rollingfile_threshold', 'ERROR')
-  $tracefile_threshold    = hiera('tracefile_threshold', 'TRACE')
+  $rollingfile_threshold = hiera('zookeeper::rollingfile_threshold', 'ERROR')
+  $tracefile_threshold    = hiera('zookeeper::tracefile_threshold', 'TRACE')
   
 }
