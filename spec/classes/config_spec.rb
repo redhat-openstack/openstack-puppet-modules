a72ac5a2070ccef9525fa3153b13b7d1f2477868
Tomas Barton <barton.tomas@gmail.com>
2013-10-16 11:12:33 +0200

    testing contents of myid file

--- spec/classes/config_spec.rb
+++ spec/classes/config_spec.rb
@@ -20,14 +20,21 @@ describe 'zookeeper::config' do
       'group'   => group,
     }) }
 
+    it { should contain_file(id_file).with({
+      'ensure'  => 'file',
+      'owner'   => user,
+      'group'   => group,
+    }).with_content(myid) }
+
   end
 
   context 'on debian-like system' do
-    let(:user) { 'zookeeper' }
-    let(:group) { 'zookeeper' }
+    let(:user)    { 'zookeeper' }
+    let(:group)   { 'zookeeper' }
     let(:cfg_dir) { '/etc/zookeeper/conf' }
     let(:log_dir) { '/var/lib/zookeeper' }
-
+    let(:id_file) { '/etc/zookeeper/conf/myid' }
+    let(:myid)    { /1/ }
 
     it_behaves_like 'debian-install', 'Debian', 'wheezy'
   end
