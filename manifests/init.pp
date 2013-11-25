30c174e3f7b4539bed663077320cc2b8ccc66b3f
Carafa, Gianni (SRF) <Gianni.Carafa@srf.ch>
2013-11-25 08:16:13 +0100

    default Config inclueds subdir

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,6 +1,16 @@
 class fluentd {
   include packages
 
+
+  file { '/etc/td-agent/td-agent.conf' :
+    ensure  => file,
+    owner   => 'root',
+    group   => 'root',
+    source  => 'puppet:///modules/fluentd/etc/fluentd/td-agent.conf',
+    require => Package['td-agent'],
+    #notify  => Class['fluentd::service'],
+  }
+
   file { '/etc/td-agent/config.d':
     ensure => "directory",
     owner  => "root",
