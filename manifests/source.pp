43466a26ad3f06c69199b01493ad8066c2502b37
Carafa, Gianni (SRF) <Gianni.Carafa@srf.ch>
2013-12-02 08:56:53 +0100

    Removed unused Code

--- manifests/source.pp
+++ manifests/source.pp
@@ -6,14 +6,6 @@ define fluentd::source (
   $config   = {},
   ) {
 
-
-  #file { $::name:
-  #      path    => "/etc/td-agent/config.d/$::name.conf",
-  #      ensure  => file,
-  #      require => Package['td-agent'],
-  #      content => template("fluentd/source.erb"),
-  #}
-
   concat::fragment { "source_$title":
     target  => "/etc/td-agent/config.d/$configfile.conf",
     require => Package['td-agent'],
