b63b62244929b7e31d31399094756bdb4d8bc8a5
Bruno Mazenauer <bruno.mazenauer@srf.ch>
2014-01-21 11:44:36 +0100

    added treasure data apt key

--- manifests/packages.pp
+++ manifests/packages.pp
@@ -6,15 +6,15 @@ class fluentd::packages {
     }
     'debian': {
 
-      # http://packages.treasure-data.com/debian/pool/contrib/t/td-agent/
-      # TODO: had to disable this part since the Repository does not provide a Key
-      
-      #apt::source { 'treasure-data':
-      #  location    => "http://packages.treasure-data.com/debian",
-      #  release     => "lucid",
-      #  repos       => "contrib",
-      #  include_src => false,
-      #}->
+      file { '/tmp/packages.treasure-data.com.key':
+        ensure => file,
+        source => 'puppet:///modules/fluentd/packages.treasure-data.com.key'
+      }->
+      exec { "import gpg key Treasure Data":
+        command => "/bin/cat /tmp/packages.treasure-data.com.key | apt-key add -",
+        unless  => "/usr/bin/apt-key list | grep -q 'Treasure Data'",
+        notify  => Class['::apt::update'],
+      }->
       package{[
         'libxslt1.1',
         'libyaml-0-2',
