fbf826206bdca34965eea10e64d0f193911840f3
Carafa, Gianni (SRF) <Gianni.Carafa@srf.ch>
2014-01-22 15:34:31 +0100

    Added test for service

---
+++ spec/classes/fluentd_service_spec.pp
@@ -0,0 +1,21 @@
+require 'spec_helper'
+
+describe 'fluentd::service', :type => :class do
+  context "On a Debian OS" do
+    let :facts do
+      {
+        :osfamily               => 'Debian',
+        :operatingsystemrelease => '7',
+        :concat_basedir         => '/tmp',
+      }
+    end
+
+    it { should contain_service("td-agent").with(
+      'ensure'     => 'running',
+      'enable'     => 'true',
+      'hasstatus'  => 'true',
+      )
+    }
+  end
+end
+
