2c599d312d0c6ed395609f93036932efade38a17
Carafa, Gianni (SRF) <Gianni.Carafa@srf.ch>
2014-08-04 16:46:44 +0200

    fix gem installation

--- manifests/install_plugin/gem.pp
+++ manifests/install_plugin/gem.pp
@@ -14,7 +14,7 @@ define fluentd::install_plugin::gem (
 
     package { $plugin_name:
       ensure   => $ensure,
-      provider => 'fluentgem',
+      provider => 'gem',
       notify   => Service["${fluentd::service_name}"];
     }
 
