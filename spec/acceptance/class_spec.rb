ab2cc2bceaf68101fd84ba4397f45415c60d38e9
Peter Souter <p.souter@kainos.com>
2014-10-02 11:58:03 +0100

    Basic class spec added
    First runs twice to check for idempotency, then curls localhost:3000 to see if Uchiwa is running

---
+++ spec/acceptance/class_spec.rb
@@ -0,0 +1,57 @@
+require 'spec_helper_acceptance'
+
+describe 'sensu class', :unless => UNSUPPORTED_PLATFORMS.include?(fact('osfamily')) do
+
+  context 'uchiwa' do
+    context 'ensure => present' do
+      it 'should work with no errors' do
+        pp = <<-EOS
+        class { 'rabbitmq':
+          ssl               => false,
+          delete_guest_user => true,
+        }
+        -> rabbitmq_vhost { 'sensu': }
+        -> rabbitmq_user  { 'sensu': password => 'secret' }
+        -> rabbitmq_user_permissions { 'sensu@sensu':
+          configure_permission => '.*',
+          read_permission      => '.*',
+          write_permission     => '.*',
+        }
+        class { 'redis': }
+        class { 'sensu':
+          server                   => true,
+          api                      => true,
+          purge_config             => true,
+          rabbitmq_password        => 'secret',
+          rabbitmq_host            => 'localhost',
+        }
+
+        class { 'uchiwa':
+          install_repo  => false,
+          require       => Class['sensu'],
+        }
+
+        uchiwa::api { 'Main Server':
+          host    => '192.168.50.4',
+          ssl     => false,
+          port    => 4567,
+          user    => 'sensu',
+          pass    => 'secret',
+          path    => '',
+          timeout => 5000
+        }
+
+        EOS
+
+        # Run it twice and test for idempotency
+        expect(apply_manifest(pp, :catch_failures => true).exit_code)
+        expect(apply_manifest(pp, :catch_failures => true).exit_code)
+      end
+      it 'should start uchiwa' do
+        shell('curl localhost:3000/') do |curl|
+          expect(curl.stdout).to include '<html lang="en" ng-app="uchiwa" ng-controller="init">'
+        end
+      end
+    end
+  end
+end
