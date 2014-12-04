caf216ba10c0a72531f45526143e850828b41e75
Crag Wolfe <cwolfe@redhat.com>
2014-12-04 05:46:58 -0500

    Automatic update
    This module update commit was generated by Bade.
    For more info please check https://github.com/paramite/bade
    
    This commit is setting modules to following state:
    galera
     - old commit: f7d4110886b643eb63dc5c347a0e8a06b09642e7
     - new commit: 061799ec2dbf1527d2bc556cac7c4d85fea106cc

--- galera/manifests/monitor.pp
+++ galera/manifests/monitor.pp
@@ -1,19 +1,43 @@
-# Class galera::monitor
+# == Class: galera::monitor
 #
-# Parameters:
-#  [*mysql_username*] - Username of the service account used for the clustercheck script.
-#  [*mysql_password*] - Password of the service account used for the clustercheck script.
-#  [*mysql_host*]     - Hostname/IP address of mysql server to monitor. Defaults to 127.0.0.1.
-#  [*mysql_port]      - Port used by mysql service. Defaults to 3306.
-#  [*monitor_port*]   - Port used by galera monitor service. Defaults to 9200.
-#  [*monitor_script*] - Full path to monitor script. Defaults to '/usr/bin/clustercheck'.
-#  [*enabled*]        - Enable/Disable galera monitor xinetd::service. Defaults to true.
+# === Parameters:
 #
-# Actions:
+# [*mysql_username*]
+#   Username of the service account used for the clustercheck script.
 #
-# Requires:
+# [*mysql_password*]
+#   Password of the service account used for the clustercheck script.
 #
-# Sample usage:
+# [*mysql_host*]
+#   Hostname/IP address of mysql server to monitor.
+#   Defaults to 127.0.0.1.
+#
+# [*mysql_port*]
+#   Port used by mysql service.
+#   Defaults to 3306.
+#
+# [*monitor_port*]
+#   Port used by galera monitor service.
+#   Defaults to 9200.
+#
+# [*monitor_script*]
+#   Full path to monitor script.\
+#   Defaults to '/usr/bin/clustercheck'.
+#
+# [*enabled*]
+#   Enable/Disable galera monitor xinetd::service.
+#   Defaults to true.
+#
+# [*create_mysql_user*]
+#   Whether to create mysql user or not.
+#   Defaults to false.
+#
+# === Actions:
+#
+# === Requires:
+#
+# === Sample usage:
+#
 # class { 'galera::monitor':
 #   mysql_username => 'mon_user',
 #   mysql_password => 'mon_pass'
@@ -38,7 +62,7 @@ class galera::monitor (
 
   file { '/etc/sysconfig/clustercheck':
     mode    => '0640',
-    content => template("galera/clustercheck.erb"),
+    content => template('galera/clustercheck.erb'),
     owner   => 'root',
     group   => 'root',
   }
@@ -58,7 +82,7 @@ class galera::monitor (
 
   if $create_mysql_user {
     mysql_user { "${mysql_username}@${mysql_host}":
-      ensure        => present, 
+      ensure        => present,
       password_hash => mysql_password($mysql_password),
       require       => [File['/root/.my.cnf'],Service['galera']],
     }
