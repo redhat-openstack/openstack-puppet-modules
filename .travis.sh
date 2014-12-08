1b13e902830cafdc080a766ee0683a90380248a4
Gael Chamoulaud <gchamoul@redhat.com>
2014-12-08 15:21:02 +0100

    Fix .travis.sh
    Signed-off-by: Gael Chamoulaud <gchamoul@redhat.com>

--- .travis.sh
+++ .travis.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -ev
-MODS="$(git diff HEAD~1 Puppetfile | grep ^+ -B2 | grep mod | cut -d"'" -f2)"
+MODS="$(git diff HEAD~1 Puppetfile | grep "^+" -B2 | grep mod | cut -d"'" -f2)"
 rake validate_puppetfile SPEC_OPTS='--format documentation --color --backtrace' || exit 1
 if [ "${MODS}" != "" ]; then
   for module in ${MODS}; do
@@ -12,7 +12,7 @@ if [ "${MODS}" != "" ]; then
     fi
   done
 else
-  echo "No changed module detected. You have to manually check whether this is OK."
+  echo "No changed module detected!"
   git diff HEAD~1 Puppetfile
-  exit 1
+  exit 0
 fi
