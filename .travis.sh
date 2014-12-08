02511b0d4c1d6d5979f3473118a827b7b0c0a968
Lukas Bezdicka <lbezdick@redhat.com>
2014-12-08 11:52:36 +0100

    Fix travis changed module detection
    If new module was added the dif contains only + and no - so from
    now we chack on ^+ in Puppetfile diff.

--- .travis.sh
+++ .travis.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
 set -ev
-MODS="$(git diff HEAD~1 Puppetfile | grep ^- -B2 | grep mod | cut -d"'" -f2)"
+MODS="$(git diff HEAD~1 Puppetfile | grep ^+ -B2 | grep mod | cut -d"'" -f2)"
 rake validate_puppetfile SPEC_OPTS='--format documentation --color --backtrace' || exit 1
-for module in ${MODS}; do
-  if [ -e ./${module}/Rakefile ]; then
-    rake test_modules[./${module}/Rakefile] SPEC_OPTS='--format documentation --color --backtrace' || exit 1
-  else
-    echo "Missing ./${module}/Rakefile, not running spec tests."
-  fi
-done
+if [ "${MODS}" != "" ]; then
+  for module in ${MODS}; do
+    if [ -e ./${module}/Rakefile ]; then
+      rake test_modules[./${module}/Rakefile] SPEC_OPTS='--format documentation --color --backtrace' || exit 1
+    else
+      echo "Missing ./${module}/Rakefile, not running spec tests."
+      exit 1
+    fi
+  done
+else
+  echo "No changed module detected."
+  git diff HEAD~1 Puppetfile
+fi
