ce430c235d5eb5c92557d767c6c8ad24692d66c3
Solly Ross <sross@redhat.com>
2014-12-16 15:40:19 -0500

    Set control_exchange in the main config file
    `control_exchange = trove` should be set in the main config file.
    
    Change-Id: I559f440a2aed76968bcdce18384f8707febb1f21

--- trove/manifests/init.pp
+++ trove/manifests/init.pp
@@ -143,6 +143,9 @@
 # [*control_exchange*]
 #   (optional) Control exchange.
 #   Defaults to 'trove'.
+# [*use_neutron*]
+#   (optional) Use Neutron
+#   Defaults to true
 #
 # [*cinder_url*]
 #   (optional) URL without the tenant segment.
@@ -183,6 +186,7 @@ class trove(
   $use_neutron                  = true,
   # DEPRECATED PARAMETERS
   $mysql_module                 = undef,
+  $use_neutron                  = true,
 ) {
   include ::trove::params
 
