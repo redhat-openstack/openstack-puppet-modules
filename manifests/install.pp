9a08219b5258a1f5c227a1687fb7c1d507286e40
Daniel Farrell <dfarrell@redhat.com>
2015-01-06 17:45:17 -0500

    Reverting to older code while tracking down Travis env bug
    Signed-off-by: Daniel Farrell <dfarrell@redhat.com>

--- manifests/install.pp
+++ manifests/install.pp
@@ -3,17 +3,8 @@
 # This class is called from opendaylight for install.
 #
 class opendaylight::install {
-  yumrepo { 'opendaylight':
-    ensure   => 'present',
-    descr    => 'OpenDaylight SDN controller',
-    baseurl  => 'http://104.131.189.230/repository/',
-    gpgcheck => False,
-    enabled  => True,
-    before   => Package['opendaylight'],
-  }
 
-  package { 'opendaylight':
-    ensure  => 'latest',
-    require => Yumrepo['opendaylight'],
+  package { $::opendaylight::package_name:
+    ensure => present,
   }
 }
