2b76bddbaea8965479428d091b21136c6e95ec63
Daniel Farrell <dfarrell@redhat.com>
2015-01-06 18:10:41 -0500

    Re-added relationship between ODL pkg and repo
    Signed-off-by: Daniel Farrell <dfarrell@redhat.com>

--- manifests/install.pp
+++ manifests/install.pp
@@ -6,14 +6,20 @@ class opendaylight::install {
 
   yumrepo { 'opendaylight':
     # 'ensure' isn't supported with Puppet <3.5
+    # hopfully it defaults to present, but docs don't say
+    # https://docs.puppetlabs.com/references/3.4.0/type.html#yumrepo
+    # https://docs.puppetlabs.com/references/3.5.0/type.html#yumrepo
     #ensure   => 'present',
     descr    => 'OpenDaylight SDN controller',
     baseurl  => 'http://104.131.189.230/repository/',
     gpgcheck => 0,
     enabled  => 1,
+    before   => Package['opendaylight'],
   }
 
   package { $::opendaylight::package_name:
     ensure => present,
+    require => Yumrepo['opendaylight'],
+
   }
 }
