ae5d28ce7fd7beea75947ae3e0cd8025e1e604b0
Daniel Farrell <dfarrell@redhat.com>
2015-01-06 16:09:24 -0500

    Added custom ODL repo, install ODL package
    Signed-off-by: Daniel Farrell <dfarrell@redhat.com>

--- manifests/install.pp
+++ manifests/install.pp
@@ -3,8 +3,17 @@
 # This class is called from opendaylight for install.
 #
 class opendaylight::install {
+  yumrepo { 'opendaylight':
+    ensure   => present,
+    descr    => 'OpenDaylight SDN controller',
+    baseurl  => 'http://104.131.189.230/repository/',
+    gpgcheck => False,
+    enabled  => True,
+    before   => Package['opendaylight'],
+  }
 
-  package { $::opendaylight::package_name:
-    ensure => present,
+  package { 'opendaylight':
+    ensure  => present,
+    require => Yumrepo['opendaylight'],
   }
 }
