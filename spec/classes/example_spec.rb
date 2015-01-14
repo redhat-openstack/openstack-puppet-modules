c86c10efd46e1f34535449e6eb59a8a54a26b468
Daniel Farrell <dfarrell@redhat.com>
2015-01-14 14:34:35 -0500

    Added CentOS tests
    Signed-off-by: Daniel Farrell <dfarrell@redhat.com>

--- spec/classes/example_spec.rb
+++ spec/classes/example_spec.rb
@@ -24,6 +24,28 @@ describe 'opendaylight' do
         it { should contain_package('opendaylight').with_ensure('present') }
       end
     end
+    ['7'].each do |lsbmajdistrelease|
+      osfamily = 'RedHat'
+      operatingsystem = 'CentOS'
+      describe "opendaylight class without any params on #{osfamily}:#{operatingsystem} #{lsbmajdistrelease}" do
+        let(:params) {{ }}
+        let(:facts) {{
+          :osfamily => osfamily,
+          :operatingsystem => operatingsystem,
+          :lsbmajdistrelease => lsbmajdistrelease,
+        }}
+
+        it { should compile.with_all_deps }
+
+        it { should contain_class('opendaylight::params') }
+        it { should contain_class('opendaylight::install').that_comes_before('opendaylight::config') }
+        it { should contain_class('opendaylight::config') }
+        it { should contain_class('opendaylight::service').that_subscribes_to('opendaylight::config') }
+
+        it { should contain_service('opendaylight') }
+        it { should contain_package('opendaylight').with_ensure('present') }
+      end
+    end
   end
 
   context 'unsupported operating system' do
