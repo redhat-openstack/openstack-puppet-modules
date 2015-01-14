2fbfe8a06dd2d43966d34994c917fb9ca40110ca
Daniel Farrell <dfarrell@redhat.com>
2015-01-14 15:01:50 -0500

    Attempting to be Puppet 2.7 compliant
    Signed-off-by: Daniel Farrell <dfarrell@redhat.com>

--- manifests/params.pp
+++ manifests/params.pp
@@ -7,14 +7,14 @@ class opendaylight::params {
   # Validate OS
   case $::operatingsystem {
     centos, redhat: {
-      unless $::lsbmajdistrelease == 7 {
+      if $::lsbmajdistrelease != 7 {
         # RHEL/CentOS versions < 7 not supported as they lack systemd
         fail("Unsupported OS: ${::operatingsystem} ${::lsbmajdistrelease}")
       }
     }
     fedora: {
       # TODO: Only need >= 15 for systemd if move to tarball or full yum repo
-      unless $::lsbmajdistrelease in [19, 20, 21] {
+      if ! ($::lsbmajdistrelease in [19, 20, 21]) {
         # Fedora versions < 19 can't be build on Copr, >21 don't exist
         fail("Unsupported OS: ${::operatingsystem} ${::lsbmajdistrelease}")
       }
