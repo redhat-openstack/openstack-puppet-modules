51a9948e3be8c2ff74975388d4bebff35425cbb8
Andrew Teixeira <teixeira@broadinstitute.org>
2015-01-15 14:09:33 -0500

    * Remove repo GPG key download and local storage...unneeded

--- manifests/repo.pp
+++ manifests/repo.pp
@@ -26,16 +26,6 @@ class zookeeper::repo(
             }
             case $osrel {
               '6': {
-                exec{ 'retrieve_clouderakey':
-                  command => "/usr/bin/curl -q http://archive.cloudera.com/cdh${cdhver}/redhat/6/${::hardwaremodel}/cdh/RPM-GPG-KEY-cloudera -o /etc/pki/rpm-gpg/RPM-GPG-KEY-cloudera",
-                  creates => '/etc/pki/rpm-gpg/RPM-GPG-KEY-cloudera',
-                }
-
-                file{ '/etc/pki/rpm-gpg/RPM-GPG-KEY-cloudera':
-                  mode => 0644,
-                  require => Exec["retrieve_clouderakey"],
-                }
-
                 yumrepo { "cloudera-cdh${cdhver}":
                   ensure   => present,
                   descr => "Cloudera's Distribution for Hadoop, Version ${cdhver}",
