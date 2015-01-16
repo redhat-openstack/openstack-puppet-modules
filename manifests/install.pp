7785ae78b3b957cfd6c465cb62e3e57b191d47bf
Daniel Farrell <dfarrell@redhat.com>
2015-01-16 10:08:05 -0500

    Install ODL via Copr repo, closes #2
    Signed-off-by: Daniel Farrell <dfarrell@redhat.com>

--- manifests/install.pp
+++ manifests/install.pp
@@ -3,15 +3,19 @@
 # This class is called from opendaylight for install.
 #
 class opendaylight::install {
+  $base_url = $::operatingsystem ? {
+    'CentOS' => 'https://copr-be.cloud.fedoraproject.org/results/dfarrell07/OpenDaylight/epel-7-$basearch/',
+    'Fedora' => 'https://copr-be.cloud.fedoraproject.org/results/dfarrell07/OpenDaylight/fedora-$releasever-$basearch/',
+  }
 
   yumrepo { 'opendaylight':
     # 'ensure' isn't supported with Puppet <3.5
     # hopfully it defaults to present, but docs don't say
     # https://docs.puppetlabs.com/references/3.4.0/type.html#yumrepo
     # https://docs.puppetlabs.com/references/3.5.0/type.html#yumrepo
-    #ensure   => 'present',
+    #ensure  => 'present',
+    baseurl  => $base_url,
     descr    => 'OpenDaylight SDN controller',
-    baseurl  => 'http://104.131.189.230/repository/',
     gpgcheck => 0,
     enabled  => 1,
     before   => Package['opendaylight'],
