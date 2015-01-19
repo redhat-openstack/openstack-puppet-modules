ab1785dfee376678ae895ffd7b9cbf85ddc97dbf
Daniel Farrell <dfarrell@redhat.com>
2015-01-19 11:16:58 -0500

    Since move to systemd, don't need to add ODL bin dir to path
    Signed-off-by: Daniel Farrell <dfarrell@redhat.com>

--- manifests/config.pp
+++ manifests/config.pp
@@ -3,9 +3,4 @@
 # This class is called from opendaylight for service config.
 #
 class opendaylight::config {
-  # TODO: Create user/group
-  file { '/etc/profile.d/opendaylight.sh':
-    mode    => '0644',
-    content => 'PATH=$PATH:/opt/opendaylight/bin',
-  }
 }
