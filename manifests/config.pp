d2d4431bfeca978a9190ff384b5bcb72438ae16e
Daniel Farrell <dfarrell@redhat.com>
2015-01-23 17:06:39 -0500

    Added template for config of Karaf features
    - Very basic, not accepting features from user yet
    - No test coverage at the moment
    
    Signed-off-by: Daniel Farrell <dfarrell@redhat.com>

--- manifests/config.pp
+++ manifests/config.pp
@@ -3,4 +3,17 @@
 # This class is called from opendaylight for service config.
 #
 class opendaylight::config {
+  # TODO: Confirm that this is a reasonable place to configure these
+  $default_features = ['config', 'standard', 'region', 'package', 'kar', 'ssh', 'management']
+
+  # TODO: Get extra features from user, add to default features
+  $features = $default_features
+
+  # This is very fragile, but I don't know of a better way to do it.
+  # Updated ODL versions will break it, as will changes to the file upstream.
+  file { 'org.apache.karaf.features.cfg':
+    ensure  => file,
+    path    => '/opt/etc/org.apache.karaf.features.cfg',
+    content => template('opendaylight/org.apache.karaf.features.cfg.erb'),
+  }
 }
