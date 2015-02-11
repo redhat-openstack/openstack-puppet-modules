5b8b1bc71c64f124af1bcb2c519a75b277e0958a
Daniel Farrell <dfarrell@redhat.com>
2015-02-11 15:57:18 -0500

    Initial refactoring of test structure
    - Currently only have OS-related tests, will add others back later
    - Currently broken, no idea why, very obtuse syntax error
    
    Signed-off-by: Daniel Farrell <dfarrell@redhat.com>

--- spec/classes/opendaylight_spec.rb
+++ spec/classes/opendaylight_spec.rb
@@ -1,186 +1,91 @@
 require 'spec_helper'
 
-describe 'opendaylight' do
-  context 'supported operating systems' do
-    osfamily = 'RedHat'
-    ['20', '21'].each do |operatingsystemmajrelease|
-      operatingsystem = 'Fedora'
-      # This is the Fedora Yum repo URL
-      yum_repo = 'https://copr-be.cloud.fedoraproject.org/results/dfarrell07/OpenDaylight/fedora-$releasever-$basearch/'
-      describe "opendaylight class on #{osfamily}:#{operatingsystem} #{operatingsystemmajrelease}" do
-        # Facts that are shared by all Fedora tests
+describe 'opendaylight class' do
+  # All tests that check OS support/not-support
+  describe 'OS support tests' do
+    # All tests for OSs in the Red Hat family (CentOS, Fedora)
+    describe 'OS family Red Hat ' do
+      let(:facts) {{
+        :osfamily => 'RedHat',
+      }}
+      # All tests for Fedora
+      describe 'Fedora' do
+        operatingsystem = 'Fedora'
         let(:facts) {{
-          :osfamily => osfamily,
           :operatingsystem => operatingsystem,
-          :operatingsystemmajrelease => operatingsystemmajrelease,
         }}
 
-        describe "using default features" do
-          # NB: This list should be the same as the one in opendaylight::params
-          default_features = ['config', 'standard', 'region', 'package', 'kar', 'ssh', 'management']
-          describe "and not passing extra features" do
-            let(:params) {{ }}
+        # This is the Fedora Yum repo URL
+        yum_repo = 'https://copr-be.cloud.fedoraproject.org/results/dfarrell07/OpenDaylight/fedora-$releasever-$basearch/'
 
-            # Run shared tests applicable to all supported OSs
-            # Note that this function is defined in spec_helper
-            supported_os_tests(yum_repo, default_features)
-          end
-
-          describe "and passing extra features" do
-            # These are real but arbitrarily chosen features
-            extra_features = ["odl-base-all", "odl-ovsdb-all"]
-            let(:params) {{
-              :extra_features => extra_features,
-            }}
-
-            # Run shared tests applicable to all supported OSs
-            # Note that this function is defined in spec_helper
-            supported_os_tests(yum_repo, default_features + extra_features)
+        # All tests for supported versions of Fedora
+        ['20', '21'].each do |operatingsystemmajrelease|
+          let(:facts) {{
+            :operatingsystemmajrelease => operatingsystemmajrelease,
+          }}
+          context "#{operatingsystemmajrelease}" do
+            # TODO: Call supported-OS-specific tests
+            it { should compile }
           end
         end
 
-        describe "overriding default features" do
-          default_features = ["standard", "ssh"]
-          describe "and not passing extra features" do
-            extra_features = []
-            let(:params) {{
-              :default_features => default_features,
-            }}
-
-            # Run shared tests applicable to all supported OSs
+        # All tests for unsupported versions of Fedora
+        ['18', '19', '22'].each do |operatingsystemmajrelease|
+          let(:facts) {{
+            :operatingsystemmajrelease => operatingsystemmajrelease,
+          }}
+          context "#{operatingsystemmajrelease}" do
+            # Run shared tests applicable to all unsupported OSs
             # Note that this function is defined in spec_helper
-            supported_os_tests(yum_repo, default_features)
-          end
-
-          describe "and passing extra features" do
-            # These are real but arbitrarily chosen features
-            extra_features = ["odl-base-all", "odl-ovsdb-all"]
-            let(:params) {{
-              :default_features => default_features,
-              :extra_features => extra_features,
-            }}
-
-            # Run shared tests applicable to all supported OSs
-            # Note that this function is defined in spec_helper
-            supported_os_tests(yum_repo, default_features + extra_features)
+            #unsupported_os_tests("Unsupported OS: #{operatingsystem} #{operatingsystemmajrelease}")
           end
         end
       end
-    end
-    ['7'].each do |operatingsystemmajrelease|
-      operatingsystem = 'CentOS'
-      # This is the CentOS 7 Yum repo URL
-      yum_repo = 'https://copr-be.cloud.fedoraproject.org/results/dfarrell07/OpenDaylight/epel-7-$basearch/'
-      describe "opendaylight class without any params on #{osfamily}:#{operatingsystem} #{operatingsystemmajrelease}" do
-        # Facts that are shared by all Fedora tests
+
+      # All tests for CentOS
+      describe 'CentOS' do
+        operatingsystem = 'CentOS'
         let(:facts) {{
-          :osfamily => osfamily,
           :operatingsystem => operatingsystem,
-          :operatingsystemmajrelease => operatingsystemmajrelease,
         }}
 
-        describe "using default features" do
-          # NB: This list should be the same as the one in opendaylight::params
-          default_features = ['config', 'standard', 'region', 'package', 'kar', 'ssh', 'management']
-          describe "and not passing extra features" do
-            let(:params) {{ }}
+        # This is the CentOS 7 Yum repo URL
+        yum_repo = 'https://copr-be.cloud.fedoraproject.org/results/dfarrell07/OpenDaylight/epel-7-$basearch/'
 
-            # Run shared tests applicable to all supported OSs
-            # Note that this function is defined in spec_helper
-            supported_os_tests(yum_repo, default_features)
-          end
-
-          describe "and passing extra features" do
-            # These are real but arbitrarily chosen features
-            extra_features = ["odl-base-all", "odl-ovsdb-all"]
-            let(:params) {{
-              :extra_features => extra_features,
-            }}
-
-            # Run shared tests applicable to all supported OSs
-            # Note that this function is defined in spec_helper
-            supported_os_tests(yum_repo, default_features + extra_features)
+        # All tests for supported versions of Fedora
+        ['7'].each do |operatingsystemmajrelease|
+          let(:facts) {{
+            :operatingsystemmajrelease => operatingsystemmajrelease,
+          }}
+          context "#{operatingsystemmajrelease}" do
+            # TODO: Call supported-OS-specific tests
           end
         end
 
-        describe "overriding default features" do
-          default_features = ["standard", "ssh"]
-          describe "and not passing extra features" do
-            extra_features = []
-            let(:params) {{
-              :default_features => default_features,
-            }}
-
-            # Run shared tests applicable to all supported OSs
+        # All tests for unsupported versions of Fedora
+        ['5', '6', '8'].each do |operatingsystemmajrelease|
+          let(:facts) {{
+            :operatingsystemmajrelease => operatingsystemmajrelease,
+          }}
+          context "#{operatingsystemmajrelease}" do
+            # Run shared tests applicable to all unsupported OSs
             # Note that this function is defined in spec_helper
-            supported_os_tests(yum_repo, default_features)
-          end
-
-          describe "and passing extra features" do
-            # These are real but arbitrarily chosen features
-            extra_features = ["odl-base-all", "odl-ovsdb-all"]
-            let(:params) {{
-              :default_features => default_features,
-              :extra_features => extra_features,
-            }}
-
-            # Run shared tests applicable to all supported OSs
-            # Note that this function is defined in spec_helper
-            supported_os_tests(yum_repo, default_features + extra_features)
+            #unsupported_os_tests("Unsupported OS: #{operatingsystem} #{operatingsystemmajrelease}")
           end
         end
       end
     end
-  end
 
-  context 'unsupported operating systems' do
-    # Test unsupported versions of Fedora
-    ['18', '19', '22'].each do |operatingsystemmajrelease|
-      osfamily = 'RedHat'
-      operatingsystem = 'Fedora'
-      describe "opendaylight class without any params on #{osfamily}:#{operatingsystem} #{operatingsystemmajrelease}" do
-        let(:facts) {{
-          :osfamily => osfamily,
-          :operatingsystem => operatingsystem,
-          :operatingsystemmajrelease => operatingsystemmajrelease,
-        }}
-
-        # Run shared tests applicable to all unsupported OSs
-        # Note that this function is defined in spec_helper
-        unsupported_os_tests("Unsupported OS: #{operatingsystem} #{operatingsystemmajrelease}")
-
-      end
-    end
-
-    # Test unsupported versions of CentOS
-    ['5', '6', '8'].each do |operatingsystemmajrelease|
-      osfamily = 'RedHat'
-      operatingsystem = 'CentOS'
-      describe "opendaylight class without any params on #{osfamily}:#{operatingsystem} #{operatingsystemmajrelease}" do
-        let(:facts) {{
-          :osfamily => osfamily,
-          :operatingsystem => operatingsystem,
-          :operatingsystemmajrelease => operatingsystemmajrelease,
-        }}
-
-        # Run shared tests applicable to all unsupported OSs
-        # Note that this function is defined in spec_helper
-        unsupported_os_tests("Unsupported OS: #{operatingsystem} #{operatingsystemmajrelease}")
-
-      end
-    end
-
-    # Test unsupported OS families
+    # All tests for unsupported OS families
     ['Debian', 'Suse', 'Solaris'].each do |osfamily|
-      describe "opendaylight class without any params on #{osfamily}" do
+      describe "OS family #{osfamily}" do
         let(:facts) {{
           :osfamily => osfamily,
         }}
 
         # Run shared tests applicable to all unsupported OSs
         # Note that this function is defined in spec_helper
-        unsupported_os_tests("Unsupported OS family: #{osfamily}")
-
+        #unsupported_os_tests("Unsupported OS family: #{osfamily}")
       end
     end
   end
