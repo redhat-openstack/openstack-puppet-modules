004933c604644732b1ec0e111f009bcbff4979e0
Daniel Farrell <dfarrell@redhat.com>
2015-02-13 10:27:59 -0500

    Refactored rspec tests now passing
    - Relevant to #43
    - The word 'class' must not appear in `describe` strings
    - `let` statements aren't inherited by child contexts
    
    Signed-off-by: Daniel Farrell <dfarrell@redhat.com>

--- spec/classes/opendaylight_spec.rb
+++ spec/classes/opendaylight_spec.rb
@@ -1,29 +1,26 @@
 require 'spec_helper'
 
-describe 'opendaylight class' do
+describe 'opendaylight' do
   # All tests that check OS support/not-support
   describe 'OS support tests' do
     # All tests for OSs in the Red Hat family (CentOS, Fedora)
     describe 'OS family Red Hat ' do
-      let(:facts) {{
-        :osfamily => 'RedHat',
-      }}
+      osfamily = 'RedHat'
       # All tests for Fedora
       describe 'Fedora' do
         operatingsystem = 'Fedora'
-        let(:facts) {{
-          :operatingsystem => operatingsystem,
-        }}
 
         # This is the Fedora Yum repo URL
         yum_repo = 'https://copr-be.cloud.fedoraproject.org/results/dfarrell07/OpenDaylight/fedora-$releasever-$basearch/'
 
         # All tests for supported versions of Fedora
         ['20', '21'].each do |operatingsystemmajrelease|
-          let(:facts) {{
-            :operatingsystemmajrelease => operatingsystemmajrelease,
-          }}
           context "#{operatingsystemmajrelease}" do
+            let(:facts) {{
+              :osfamily => osfamily,
+              :operatingsystem => operatingsystem,
+              :operatingsystemmajrelease => operatingsystemmajrelease,
+            }}
             # TODO: Call supported-OS-specific tests
             it { should compile }
           end
@@ -31,13 +28,15 @@ describe 'opendaylight class' do
 
         # All tests for unsupported versions of Fedora
         ['18', '19', '22'].each do |operatingsystemmajrelease|
-          let(:facts) {{
-            :operatingsystemmajrelease => operatingsystemmajrelease,
-          }}
           context "#{operatingsystemmajrelease}" do
+            let(:facts) {{
+              :osfamily => osfamily,
+              :operatingsystem => operatingsystem,
+              :operatingsystemmajrelease => operatingsystemmajrelease,
+            }}
             # Run shared tests applicable to all unsupported OSs
             # Note that this function is defined in spec_helper
-            #unsupported_os_tests("Unsupported OS: #{operatingsystem} #{operatingsystemmajrelease}")
+            unsupported_os_tests("Unsupported OS: #{operatingsystem} #{operatingsystemmajrelease}")
           end
         end
       end
@@ -45,32 +44,34 @@ describe 'opendaylight class' do
       # All tests for CentOS
       describe 'CentOS' do
         operatingsystem = 'CentOS'
-        let(:facts) {{
-          :operatingsystem => operatingsystem,
-        }}
 
         # This is the CentOS 7 Yum repo URL
         yum_repo = 'https://copr-be.cloud.fedoraproject.org/results/dfarrell07/OpenDaylight/epel-7-$basearch/'
 
-        # All tests for supported versions of Fedora
+        # All tests for supported versions of CentOS
         ['7'].each do |operatingsystemmajrelease|
-          let(:facts) {{
-            :operatingsystemmajrelease => operatingsystemmajrelease,
-          }}
           context "#{operatingsystemmajrelease}" do
+            let(:facts) {{
+              :osfamily => osfamily,
+              :operatingsystem => operatingsystem,
+              :operatingsystemmajrelease => operatingsystemmajrelease,
+            }}
             # TODO: Call supported-OS-specific tests
+            it { should compile }
           end
         end
 
-        # All tests for unsupported versions of Fedora
+        # All tests for unsupported versions of CentOS
         ['5', '6', '8'].each do |operatingsystemmajrelease|
-          let(:facts) {{
-            :operatingsystemmajrelease => operatingsystemmajrelease,
-          }}
           context "#{operatingsystemmajrelease}" do
+            let(:facts) {{
+              :osfamily => osfamily,
+              :operatingsystem => operatingsystem,
+              :operatingsystemmajrelease => operatingsystemmajrelease,
+            }}
             # Run shared tests applicable to all unsupported OSs
             # Note that this function is defined in spec_helper
-            #unsupported_os_tests("Unsupported OS: #{operatingsystem} #{operatingsystemmajrelease}")
+            unsupported_os_tests("Unsupported OS: #{operatingsystem} #{operatingsystemmajrelease}")
           end
         end
       end
@@ -78,14 +79,14 @@ describe 'opendaylight class' do
 
     # All tests for unsupported OS families
     ['Debian', 'Suse', 'Solaris'].each do |osfamily|
-      describe "OS family #{osfamily}" do
+      context "OS family #{osfamily}" do
         let(:facts) {{
           :osfamily => osfamily,
         }}
 
         # Run shared tests applicable to all unsupported OSs
         # Note that this function is defined in spec_helper
-        #unsupported_os_tests("Unsupported OS family: #{osfamily}")
+        unsupported_os_tests("Unsupported OS family: #{osfamily}")
       end
     end
   end
