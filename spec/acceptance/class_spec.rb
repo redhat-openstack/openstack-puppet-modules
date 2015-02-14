03c3d11b355d3be4e09ce49412a0bf907662cdf3
Daniel Farrell <dfarrell@redhat.com>
2015-02-14 08:56:16 -0500

    Initial Beaker test for tarball install
    - Relevant to #45
    - Found #51
    
    Signed-off-by: Daniel Farrell <dfarrell@redhat.com>

--- spec/acceptance/class_spec.rb
+++ spec/acceptance/class_spec.rb
@@ -64,8 +64,8 @@ describe 'opendaylight class' do
     end
   end
 
-  context "testing Karaf config file" do
-    context "using default features" do
+  describe "testing Karaf config file" do
+    describe "using default features" do
       # NB: This list should be the same as the one in opendaylight::params
       default_features = ['config', 'standard', 'region', 'package', 'kar', 'ssh', 'management']
       context "and not passing extra features" do
@@ -105,7 +105,7 @@ describe 'opendaylight class' do
       end
     end
 
-    context "overriding default features" do
+    describe "overriding default features" do
       default_features = ["standard", "ssh"]
       context "and not passing extra features" do
         # Using puppet_apply as a helper
@@ -145,6 +145,55 @@ describe 'opendaylight class' do
         # Punt validations to shared fn in spec_helper_acceptance
         validate_karaf_config(default_features + extra_features)
       end
+    end
+  end
+
+  # All tests for tarball install method
+  describe "tarball install method" do
+    pp = <<-EOS
+    class { 'opendaylight':
+      install_method => 'tarball'
+    }
+    EOS
+    # TODO: Pull logic shared between install methods into helper fn
+    # TODO: This logic is dup'd above, do this^^ and remove it
+
+    # TODO: It'd be nice to do this independently of install dir name
+    describe file('/opt/opendaylight-0.2.2/') do
+      it { should be_directory }
+      it { should be_owned_by 'odl' }
+      it { should be_grouped_into 'odl' }
+      it { should be_mode '775' }
+    end
+
+    describe service('opendaylight') do
+      it { should be_enabled }
+      it { should be_enabled.with_level(3) }
+      it { should be_running }
+    end
+
+    # OpenDaylight will appear as a Java process
+    describe process('java') do
+      it { should be_running }
+    end
+
+    # TODO: It'd be nice to do this independently of install dir name
+    describe user('odl') do
+      it { should exist }
+      it { should belong_to_group 'odl' }
+      it { should have_home_directory '/opt/opendaylight-0.2.2' }
+    end
+
+    describe file('/home/odl') do
+      # Home dir shouldn't be created for odl user
+      it { should_not be_directory }
+    end
+
+    describe file('/usr/lib/systemd/system/opendaylight.service') do
+      it { should be_file }
+      it { should be_owned_by 'root' }
+      it { should be_grouped_into 'root' }
+      it { should be_mode '644' }
     end
   end
 end
