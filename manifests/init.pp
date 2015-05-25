4317759c558d50b0f4e1dee4d561d03d0224b315
dallinb <b.dalling@locp.co.uk>
2015-05-25 09:54:09 +0100

    Carries out a very basic install.

--- manifests/init.pp
+++ manifests/init.pp
@@ -35,7 +35,42 @@
 #
 # Copyright 2015 Your name here, unless otherwise noted.
 #
-class cassandra {
+class cassandra (
+  $cluster_name                  = undef,
+  $cassandra_package_name        = 'dsc21',
+  $cassandra_package_ensure      = 'present',
+  $cassandra_opt_package_name    = undef,
+  $cassandra_opt_package_ensure  = 'present',
+  $java_package_name             = undef,
+  $java_package_ensure           = 'present',
+  $manage_dsc_repo               = false,
+  ) {
 
+  if $manage_dsc_repo == true {
+    yumrepo { 'datastax':
+      ensure   => present,
+      descr    => 'DataStax Repo for Apache Cassandra',
+      baseurl  => 'http://rpm.datastax.com/community',
+      enabled  => 1,
+      gpgcheck => 0,
+    }
+  }
 
+  if $java_package_name != undef {
+    package { $java_package_name:
+      ensure => $java_package_ensure,
+      before => Package[ $cassandra_package_name ],
+    }
+  }
+
+  package { $cassandra_package_name:
+    ensure => $cassandra_package_ensure,
+  }
+
+  if $cassandra_opt_package_name != 'undef' {
+    package { $cassandra_opt_package_name:
+      ensure  => $cassandra_opt_package_ensure,
+      require => Package[$cassandra_package_name],
+    }
+  }
 }
