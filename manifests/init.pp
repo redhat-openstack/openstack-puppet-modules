a803a2b3fcb1ec920c3561a64cc4888ebd879e6f
dallinb <b.dalling@locp.co.uk>
2015-05-25 11:32:59 +0100

    All documentation is now in the module README.

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,39 +1,7 @@
 # == Class: cassandra
 #
-# Full description of class cassandra here.
-#
-# === Parameters
-#
-# Document parameters here.
-#
-# [*sample_parameter*]
-#   Explanation of what this parameter affects and what it defaults to.
-#   e.g. "Specify one or more upstream ntp servers as an array."
-#
-# === Variables
-#
-# Here you should define a list of variables that this module would require.
-#
-# [*sample_variable*]
-#   Explanation of how this variable affects the funtion of this class and if
-#   it has a default. e.g. "The parameter enc_ntp_servers must be set by the
-#   External Node Classifier as a comma separated list of hostnames." (Note,
-#   global variables should be avoided in favor of class parameters as
-#   of Puppet 2.6.)
-#
-# === Examples
-#
-#  class { 'cassandra':
-#    servers => [ 'pool.ntp.org', 'ntp.local.company.com' ],
-#  }
-#
-# === Authors
-#
-# Author Name <author@domain.com>
-#
-# === Copyright
-#
-# Copyright 2015 Your name here, unless otherwise noted.
+# Please see the README for this module for full details of what this class
+# does as part of the module and how to use it.
 #
 class cassandra (
   $cluster_name                  = undef,
@@ -46,14 +14,21 @@ class cassandra (
   $manage_dsc_repo               = false,
   ) {
 
-  if $manage_dsc_repo == true {
-    yumrepo { 'datastax':
-      ensure   => present,
-      descr    => 'DataStax Repo for Apache Cassandra',
-      baseurl  => 'http://rpm.datastax.com/community',
-      enabled  => 1,
-      gpgcheck => 0,
-      before   => Package[ $cassandra_package_name ],
+  case $::osfamily {
+    'RedHat': {
+      if $manage_dsc_repo == true {
+        yumrepo { 'datastax':
+          ensure   => present,
+          descr    => 'DataStax Repo for Apache Cassandra',
+          baseurl  => 'http://rpm.datastax.com/community',
+          enabled  => 1,
+          gpgcheck => 0,
+          before   => Package[ $cassandra_package_name ],
+        }
+      }
+    }
+    default: {
+      fail("OS family ${::osfamily} not supported")
     }
   }
 
