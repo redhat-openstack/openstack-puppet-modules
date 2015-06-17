55cd08ecda5b277342b6b87817022ce7ed740f50
dallinb <b.dalling@locp.co.uk>
2015-06-17 09:05:53 +0100

    Adding more checks to testing (#8).

--- spec/classes/cassandra_spec.rb
+++ spec/classes/cassandra_spec.rb
@@ -15,6 +15,58 @@ describe 'cassandra' do
     }
   end
 
+  context 'On a RedHat OS with manage_dsc_repo set to true' do
+    let :facts do
+      {
+        :osfamily => 'RedHat'
+      }
+    end
+
+    let :params do
+      {
+        :manage_dsc_repo => true
+      }
+    end
+
+    it {
+      should contain_yumrepo('datastax')
+    }
+  end
+
+  context 'On a Debian OS with defaults for all parameters' do
+    let :facts do
+      {
+        :osfamily => 'Debian'
+      }
+    end
+
+    it {
+      should contain_class('cassandra')
+      should contain_class('cassandra::install')
+      should contain_class('cassandra::config')
+    }
+  end
+
+  context 'On a Debian OS with manage_dsc_repo set to true' do
+    let :facts do
+      {
+        :osfamily => 'Debian',
+        :lsbdistid => 'Ubuntu',
+        :lsbdistrelease => '14.04'
+      }
+    end
+
+    let :params do
+      {
+        :manage_dsc_repo => true
+      }
+    end
+
+    it {
+      should contain_class('apt')
+    }
+  end
+
   context 'On an unknown OS with defaults for all parameters' do
     let :facts do
       {
