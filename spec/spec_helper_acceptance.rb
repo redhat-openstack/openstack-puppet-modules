94078b4ac167851b7cfd0cde7980d4433b631e36
dallinb <b.dalling@locp.co.uk>
2015-06-17 18:04:44 +0100

    We need apt which has a dependency on stdlib (#8).

--- spec/spec_helper_acceptance.rb
+++ spec/spec_helper_acceptance.rb
@@ -17,6 +17,7 @@ RSpec.configure do |c|
     puppet_module_install(:source => module_root, :module_name => 'cassandra')
     hosts.each do |host|
       on host, puppet('module','install','puppetlabs-stdlib'), { :acceptable_exit_codes => [0,1] }
+      on host, puppet('module','install','puppetlabs-apt'), { :acceptable_exit_codes => [0,1] }
     end
   end
 end
