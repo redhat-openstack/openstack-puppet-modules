348d71ac6df8adb3b7c681fbde2c7cf6f398e4af
dallinb <b.dalling@locp.co.uk>
2015-06-20 00:19:38 +0100

    Ensure Java is installed, but don't test services are running (#17).

--- spec/acceptance/cassandra_spec.rb
+++ spec/acceptance/cassandra_spec.rb
@@ -7,15 +7,18 @@ describe 'cassandra class' do
       pp = <<-EOS
         if $::osfamily == 'Debian' {
           class { 'cassandra':
-            config_path           => '/etc/cassandra',
-            datastax_agent_ensure => 'present',
-            manage_dsc_repo       => true,
-            java_package_name     => 'openjdk-7-jre-headless',
+            config_path                   => '/etc/cassandra',
+            datastax_agent_ensure         => 'present',
+            datastax_agent_manage_service => false,
+            manage_dsc_repo               => true,
+            java_package_name             => 'openjdk-7-jre-headless',
           }
         } else {
           class { 'cassandra':
-            datastax_agent_ensure => 'present',
-            manage_dsc_repo       => true,
+            datastax_agent_ensure         => 'present',
+            datastax_agent_manage_service => false,
+            manage_dsc_repo               => true,
+            java_package_name             => 'java-1.7.0-openjdk',
           }
         }
       EOS
