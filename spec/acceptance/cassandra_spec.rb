55b01f83febf67fcf31f05d68d435e446d757742
dallinb <b.dalling@locp.co.uk>
2015-06-26 22:31:17 +0100

    Working now (#21).

--- spec/acceptance/cassandra_spec.rb
+++ spec/acceptance/cassandra_spec.rb
@@ -1,8 +1,7 @@
 require 'spec_helper_acceptance'
 
 describe 'cassandra class' do
-  describe 'running puppet code' do
-    # Using puppet_apply as a helper
+  describe 'Install Cassandra with Java.' do
     it 'should work with no errors' do
       pp = <<-EOS
         if $::osfamily == 'Debian' {
@@ -12,12 +11,22 @@ describe 'cassandra class' do
         }
 
         class { 'cassandra':
+          java_package_name => $java_package_name,
+          manage_dsc_repo   => true,
+        }
+      EOS
+
+      # Run it twice and test for idempotency
+      apply_manifest(pp, :catch_failures => true)
+      expect(apply_manifest(pp, :catch_failures => true).exit_code).to be_zero
+    end
+  end
+
+  describe 'Install the DataStax Agent.' do
+    it 'should work with no errors' do
+      pp = <<-EOS
+        class { 'cassandra':
           datastax_agent_package_ensure => 'present',
-          datastax_agent_service_enable => 'false',
-          datastax_agent_service_ensure => 'stopped',
-          manage_dsc_repo               => true,
-          service_enable                => 'false',
-          service_ensure                => 'stopped',
         }
       EOS
 
