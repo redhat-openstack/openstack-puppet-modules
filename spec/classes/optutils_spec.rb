0ca0ede28f86fb068969cba10e389d6d9ede4e90
dallinb <b.dalling@locp.co.uk>
2015-07-12 11:28:21 +0100

    Added spec test for the new optutils sub-class (#27).

---
+++ spec/classes/optutils_spec.rb
@@ -0,0 +1,58 @@
+require 'spec_helper'
+describe 'cassandra::optutils' do
+
+  context 'On a RedHat OS with defaults for all parameters' do
+    let :facts do
+      {
+        :osfamily => 'RedHat'
+      }
+    end
+
+    it {
+      should contain_class('cassandra::optutils')
+      should contain_package('cassandra21-tools')
+    }
+  end
+
+  context 'On an Ubuntu OS with defaults for all parameters' do
+    let :facts do
+      {
+        :operatingsystem => 'Ubuntu'
+      }
+    end
+
+    it {
+      should contain_class('cassandra::optutils')
+      should contain_package('cassandra-tools')
+    }
+  end
+
+  context 'With java_package_name set to foobar' do
+    let :params do
+      {
+        :package_name   => 'foobar-java',
+        :ensure         => '42',
+      }
+    end
+
+    it {
+      should contain_package('foobar-java').with({
+        :ensure => 42,
+      })
+    }
+  end
+
+  context 'On an unknown OS with defaults for all parameters' do
+    let :facts do
+      {
+        :osfamily => 'Darwin'
+      }
+    end
+
+    it {
+      expect {
+        should raise_error(Puppet::Error)
+      }
+    }
+  end
+end
