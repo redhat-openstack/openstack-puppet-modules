17eaa4906e5a439e03c400b6cbf183952c1b2f8f
dallinb <b.dalling@locp.co.uk>
2015-07-12 11:29:12 +0100

    A new sub-class for the optional tools.

---
+++ manifests/optutils.pp
@@ -0,0 +1,22 @@
+# Please see the README file for the module.
+class cassandra::optutils (
+  $ensure       = 'present',
+  $package_name = 'undef'
+  ) {
+  if $package_name == 'undef' {
+    if $::osfamily == 'RedHat' {
+      $real_package_name = 'cassandra21-tools'
+    } elsif $::operatingsystem == 'Ubuntu' {
+      $real_package_name = 'cassandra-tools'
+    } else {
+      fail("OS family ${::osfamily} not supported")
+    }
+  } else {
+    $real_package_name = $package_name
+  }
+
+  package { $real_package_name:
+    ensure  => $ensure,
+    require => Class['cassandra']
+  }
+}
