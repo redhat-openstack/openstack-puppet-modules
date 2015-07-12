8e177301df08625ebb10b6d72c109c45b2316187
dallinb <b.dalling@locp.co.uk>
2015-07-12 09:48:25 +0100

    New manifest and associated tests (#26).

---
+++ manifests/java.pp
@@ -0,0 +1,21 @@
+# Please see the README file for the module.
+class cassandra::java (
+  $ensure       = 'present',
+  $package_name = 'undef'
+  ) {
+  if $package_name == 'undef' {
+    if $::osfamily == 'RedHat' {
+      $java_package_name = 'java-1.7.0-openjdk-headless'
+    } elsif $::operatingsystem == 'Ubuntu' {
+      $java_package_name = 'openjdk-7-jre-headless'
+    } else {
+      fail("OS family ${::osfamily} not supported")
+    }
+  } else {
+    $java_package_name = $package_name
+  }
+
+  package { $java_package_name:
+    ensure => $ensure
+  }
+}
---
+++ spec/classes/java_spec.rb
@@ -0,0 +1,57 @@
+require 'spec_helper'
+describe 'cassandra::java' do
+
+  context 'On a RedHat OS with defaults for all parameters' do
+    let :facts do
+      {
+        :osfamily => 'RedHat'
+      }
+    end
+
+    it {
+      should contain_class('cassandra::java')
+      should contain_package('java-1.7.0-openjdk-headless')
+    }
+  end
+
+  context 'On an Ubuntu OS with defaults for all parameters' do
+    let :facts do
+      {
+        :operatingsystem => 'Ubuntu'
+      }
+    end
+
+    it {
+      should contain_class('cassandra::java')
+      should contain_package('openjdk-7-jre-headless')
+    }
+  end
+
+  context 'With java_package_name set to foobar' do
+      {
+        :java_package_name   => 'foobar-java',
+        :java_package_ensure => '42',
+      }
+    end
+
+    it {
+      should contain_package('foobar-java').with({
+        :ensure => 42,
+      })
+    }
+  end
+
+  context 'On an unknown OS with defaults for all parameters' do
+    let :facts do
+      {
+        :osfamily => 'Darwin'
+      }
+    end
+
+    it {
+      expect {
+        should raise_error(Puppet::Error)
+      }
+    }
+  end
+end
