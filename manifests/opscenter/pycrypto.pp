0cf4b8186bc54d86629009de698ed0f20cd8f0f0
dallinb <b.dalling@locp.co.uk>
2015-07-19 08:57:13 +0100

    Allow the pycrypto to install EPEL if required (#18).

--- manifests/opscenter/pycrypto.pp
+++ manifests/opscenter/pycrypto.pp
@@ -1,11 +1,19 @@
 # Class ::cassandra::opscenter::pycrypto
 class cassandra::opscenter::pycrypto (
   $ensure       = 'present',
+  $manage_epel  = false,
   $package_name = 'pycrypto',
   $reqd_pckgs   = ['python-devel', 'python-pip' ],
   $provider     = 'pip',
   ){
   if $::osfamily == 'RedHat' {
+    if $manage_epel == true {
+      package { 'epel-release':
+        ensure => 'present',
+        before => Package[ $reqd_pckgs ]
+      }
+    }
+
     package { $reqd_pckgs:
       ensure => present,
       before => Package[$package_name]
