64646f3a110f0ac9c8611100b09c681551a375b4
dallinb <b.dalling@locp.co.uk>
2015-07-19 08:20:14 +0100

    First stab at a pycrypto class for Red Hat (#18).

--- spec/acceptance/cassandra_spec.rb
+++ spec/acceptance/cassandra_spec.rb
@@ -2,11 +2,7 @@ require 'spec_helper_acceptance'
 
 describe 'cassandra class' do
   pp1 = <<-EOS
-    class { 'cassandra':
-      manage_dsc_repo   => true,
-    }
-
-    include '::cassandra::java'
+    include ::cassandra::opscenter::pycrypto
   EOS
 
   describe 'Install Cassandra with Java.' do
@@ -14,54 +10,4 @@ describe 'cassandra class' do
       apply_manifest(pp1, :catch_failures => true)
     end
   end
-
-  pp2 = <<-EOS
-    include '::cassandra'
-    include '::cassandra::optutils'
-  EOS
-
-  describe 'Install the Optional Cassandra tools.' do
-    it 'should work with no errors' do
-      apply_manifest(pp2, :catch_failures => true)
-    end
-  end
-
-  pp3 = <<-EOS
-    include '::cassandra'
-    include '::cassandra::datastax_agent'
-  EOS
-
-  describe 'Install the DataStax Agent.' do
-    it 'should work with no errors' do
-      apply_manifest(pp3, :catch_failures => true)
-    end
-  end
-
-  pp4 = <<-EOS
-    class { 'cassandra':
-      manage_dsc_repo              => true,
-      cassandra_opt_package_ensure => 'present',
-    }
-
-    include '::cassandra::datastax_agent'
-    include '::cassandra::java'
-    include '::cassandra::optutils'
-  EOS
-
-  describe 'Idempotency test.' do
-    it 'should work with no errors' do
-      expect(apply_manifest(pp3, :catch_failures => true).exit_code).to be_zero
-    end
-  end
-
-  describe service('cassandra') do
-    it { is_expected.to be_running }
-    it { is_expected.to be_enabled }
-  end
-
-  describe service('datastax-agent') do
-    it { is_expected.to be_running }
-    it { is_expected.to be_enabled }
-  end
-
 end
