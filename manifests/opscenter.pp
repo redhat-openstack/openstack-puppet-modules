1f4d020a3bd7771d2c7114bc23eef2322358e1e0
dallinb <b.dalling@locp.co.uk>
2015-07-22 21:21:01 +0100

    Rename of the class parameters (#57).

--- manifests/opscenter.pp
+++ manifests/opscenter.pp
@@ -2,19 +2,22 @@
 #
 # See the module README for details on how to use.
 class cassandra::opscenter (
-    $authentication_enabled = 'False',
-    $ensure                 = present,
-    $config_file            = '/etc/opscenter/opscenterd.conf',
-    $interface              = '0.0.0.0',
-    $package_name           = 'opscenter',
-    $port                   = 8888,
-    $service_enable         = true,
-    $service_ensure         = 'running',
-    $service_name           = 'opscenterd',
-    $ssl_keyfile            = undef,
-    $ssl_certfile           = undef,
-    $ssl_port               = undef,
-    $stat_reporter_interval = undef
+    $authentication_enabled            = 'False',
+    $ensure                            = present,
+    $config_file                       = '/etc/opscenter/opscenterd.conf',
+    $package_name                      = 'opscenter',
+    $service_enable                    = true,
+    $service_ensure                    = 'running',
+    $service_name                      = 'opscenterd',
+    $stat_reporter_interval            = undef,
+    $webserver_interface               = '0.0.0.0',
+    $webserver_port                    = 8888,
+    $webserver_ssl_certfile            = undef,
+    $webserver_ssl_keyfile             = undef,
+    $webserver_ssl_port                = undef,
+    $webserver_staticdir               = undef,
+    $webserver_sub_process_timeout     = undef,
+    $webserver_tarball_process_timeout = undef
   ) {
   package { 'opscenter':
     ensure  => $ensure,
@@ -34,35 +37,35 @@ class cassandra::opscenter (
     path    => $config_file,
     section => 'webserver',
     setting => 'port',
-    value   => $port
+    value   => $webserver_port
   }
 
   cassandra::opscenter::setting { 'webserver interface':
     path    => $config_file,
     section => 'webserver',
     setting => 'interface',
-    value   => $interface
+    value   => $webserver_interface
   }
 
   cassandra::opscenter::setting { 'webserver ssl_keyfile':
     path    => $config_file,
     section => 'webserver',
     setting => 'ssl_keyfile',
-    value   => $ssl_keyfile
+    value   => $webserver_ssl_keyfile
   }
 
   cassandra::opscenter::setting { 'webserver ssl_certfile':
     path    => $config_file,
     section => 'webserver',
     setting => 'ssl_certfile',
-    value   => $ssl_certfile
+    value   => $webserver_ssl_certfile
   }
 
   cassandra::opscenter::setting { 'webserver ssl_port':
     path    => $config_file,
     section => 'webserver',
     setting => 'ssl_port',
-    value   => $ssl_port
+    value   => $webserver_ssl_port
   }
 
   service { 'opscenterd':
