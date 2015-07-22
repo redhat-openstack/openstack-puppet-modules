2f5ed475d7d2c1f145bb92246ef0496fb84518f1
dallinb <b.dalling@locp.co.uk>
2015-07-22 17:29:19 +0100

    A start on a spec test harness for OpsCenter (#18).

---
+++ spec/defines/opscenter/setting_spec.rb
@@ -0,0 +1,26 @@
+require 'spec_helper'
+describe 'cassandra::opscenter::setting' do
+  let(:pre_condition) { [
+    'define ini_setting($ensure, $path, $section, $key_val_separator, $setting, $value) {}'
+  ] }
+
+  context 'Test that settings can be set.' do
+    let(:title) { 'section:setting' }
+    let :params do
+      {
+        :service_name => 'service_name',
+        :path         => '/path/to/file',
+        :section      => 'section',
+        :setting      => 'setting',
+        :value        => 'value',
+      }
+    end
+
+    it {
+      should contain_ini_setting('section:setting').with({
+        'ensure' => 'present',
+        'path' => '/path/to/file',
+      })
+    }
+  end
+end
