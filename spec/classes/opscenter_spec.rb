dc2362e6c8f6d920ee507eed6a7f765ce1bc6996
dallinb <b.dalling@locp.co.uk>
2015-07-24 20:44:54 +0100

    Added spec tests for webserver (#57).

--- spec/classes/opscenter_spec.rb
+++ spec/classes/opscenter_spec.rb
@@ -37,4 +37,103 @@ describe 'cassandra::opscenter' do
       should contain_service('opscenterd')
     }
   end
+
+  context 'Test webserver interface.' do
+    it {
+      should contain_cassandra__opscenter__setting('webserver interface').with({
+        'path'         => '/etc/opscenter/opscenterd.conf',
+        'section'      => 'webserver',
+        'setting'      => 'interface',
+        'value'        => '0.0.0.0'
+      })
+    }
+  end
+
+  context 'Test webserver log_path.' do
+    it {
+      should contain_cassandra__opscenter__setting('webserver log_path').with({
+        'path'         => '/etc/opscenter/opscenterd.conf',
+        'section'      => 'webserver',
+        'setting'      => 'log_path',
+        'value'        => nil
+      })
+    }
+  end
+
+  context 'Test webserver port.' do
+    it {
+      should contain_cassandra__opscenter__setting('webserver port').with({
+        'path'         => '/etc/opscenter/opscenterd.conf',
+        'section'      => 'webserver',
+        'setting'      => 'port',
+        'value'        => 8888
+      })
+    }
+  end
+
+  context 'Test webserver ssl_certfile.' do
+    it {
+      should contain_cassandra__opscenter__setting('webserver ssl_certfile').with({
+        'path'         => '/etc/opscenter/opscenterd.conf',
+        'section'      => 'webserver',
+        'setting'      => 'ssl_certfile',
+        'value'        => nil
+      })
+    }
+  end
+
+  context 'Test webserver ssl_keyfile.' do
+    it {
+      should contain_cassandra__opscenter__setting('webserver ssl_keyfile').with({
+        'path'         => '/etc/opscenter/opscenterd.conf',
+        'section'      => 'webserver',
+        'setting'      => 'ssl_keyfile',
+        'value'        => nil
+      })
+    }
+  end
+
+  context 'Test webserver ssl_port.' do
+    it {
+      should contain_cassandra__opscenter__setting('webserver ssl_port').with({
+        'path'         => '/etc/opscenter/opscenterd.conf',
+        'section'      => 'webserver',
+        'setting'      => 'ssl_port',
+        'value'        => nil
+      })
+    }
+  end
+
+  context 'Test webserver staticdir.' do
+    it {
+      should contain_cassandra__opscenter__setting('webserver staticdir').with({
+        'path'         => '/etc/opscenter/opscenterd.conf',
+        'section'      => 'webserver',
+        'setting'      => 'staticdir',
+        'value'        => nil
+      })
+    }
+  end
+
+  context 'Test webserver sub_process_timeout.' do
+    it {
+      should contain_cassandra__opscenter__setting('webserver sub_process_timeout').with({
+        'path'         => '/etc/opscenter/opscenterd.conf',
+        'section'      => 'webserver',
+        'setting'      => 'sub_process_timeout',
+        'value'        => nil
+      })
+    }
+  end
+
+  context 'Test webserver tarball_process_timeout.' do
+    it {
+      should contain_cassandra__opscenter__setting('webserver tarball_process_timeout').with({
+        'path'         => '/etc/opscenter/opscenterd.conf',
+        'section'      => 'webserver',
+        'setting'      => 'tarball_process_timeout',
+        'value'        => nil
+      })
+    }
+  end
 end
