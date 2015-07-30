985678866ed66f789f7c6abc5002dfb2f581adde
Robin Bowes <robin.bowes@yo61.com>
2015-07-30 11:17:54 +0100

    Fix template_body so it correctly reports the template filename if it is not found

--- lib/puppet/parser/functions/template_body.rb
+++ lib/puppet/parser/functions/template_body.rb
@@ -1,7 +1,7 @@
 Puppet::Parser::Functions::newfunction(:template_body, :type => :rvalue) do |args|
   args.collect do |file|
     unless filename = Puppet::Parser::Files.find_template(file, self.compiler.environment)
-      raise Puppet::ParseError, "Could not find template '#{filename}'"
+      raise Puppet::ParseError, "Could not find template '#{file}'"
     end
     File.read(filename)
   end.join('')
