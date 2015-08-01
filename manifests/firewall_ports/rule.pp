e5565300a1804d6790435cf5d98476316c71c127
dallinb <b.dalling@locp.co.uk>
2015-08-01 09:30:22 +0100

    An attempt to fix Coveralls complaints (#64).

--- manifests/firewall_ports/rule.pp
+++ manifests/firewall_ports/rule.pp
@@ -4,10 +4,14 @@ define cassandra::firewall_ports::rule(
     $port,
     $source = $title,
   ) {
+  # Need the following code block to excluded from Coveralls.io analysis
+  # because we're not responsible for PuppetLabs code.
+  # :nocov:
   Firewall { "${port} - ${source}":
     action => $action,
     port   => $port,
     proto  => 'tcp',
     source => $source
   }
+  # :nocov:
 }
