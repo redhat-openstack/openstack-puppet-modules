7d92868ddd087d08213ce51b0630139ec4ac160f
dallinb <b.dalling@locp.co.uk>
2015-08-01 09:47:13 +0100

    Default rule is accept and made start on OpsCenter firewall (#64).

--- manifests/firewall_ports.pp
+++ manifests/firewall_ports.pp
@@ -5,12 +5,16 @@ class cassandra::firewall_ports (
   $client_subnets     = ['0.0.0.0/0'],
   $inter_node_subnets = ['0.0.0.0/0'],
   $public_subnets     = ['0.0.0.0/0'],
-  $ssh_action         = 'accept',
   $ssh_port           = 22,
   $opscenter_subnets  = ['0.0.0.0/0']
   ) {
   cassandra::firewall_ports::rule { $public_subnets:
-    action => $ssh_action,
     port   => $ssh_port,
+  }
+
+  if $::cassandra::opscenter::webserver_port != undef {
+    cassandra::firewall_ports::rule { $public_subnets:
+      port   => $::cassandra::opscenter::webserver_port,
+    }
   }
 }
