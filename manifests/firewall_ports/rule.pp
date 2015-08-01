2c91e25ac23394ed602457f237f6c01f575c20d8
dallinb <b.dalling@locp.co.uk>
2015-08-01 09:46:27 +0100

    Removed nocov directive (didn't work) and the default rule is to now accept (#64).

--- manifests/firewall_ports/rule.pp
+++ manifests/firewall_ports/rule.pp
@@ -1,17 +1,12 @@
 # cassandra::firewall_ports::rule
 define cassandra::firewall_ports::rule(
-    $action,
     $port,
     $source = $title,
   ) {
-  # Need the following code block to excluded from Coveralls.io analysis
-  # because we're not responsible for PuppetLabs code.
-  # :nocov:
   Firewall { "${port} - ${source}":
-    action => $action,
+    action => 'accept',
     port   => $port,
     proto  => 'tcp',
     source => $source
   }
-  # :nocov:
 }
