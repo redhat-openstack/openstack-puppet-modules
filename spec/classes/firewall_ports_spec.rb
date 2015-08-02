926a10052f6b64b3372a43a3762ec0b2d1bb0061
dallinb <b.dalling@locp.co.uk>
2015-08-02 11:18:41 +0100

    Some more work on spec tests on the firewall_ports (#64).

--- spec/classes/firewall_ports_spec.rb
+++ spec/classes/firewall_ports_spec.rb
@@ -2,8 +2,22 @@ require 'spec_helper'
 describe 'cassandra::firewall_ports' do
   let(:pre_condition) { [
     'define firewall ($action, $port, $proto, $source) {}',
-    'function prefix ($a, $p) { }'
   ] }
+
+  let!(:does_something) { MockFunction.new('prefix') { |f|
+      f.stubs(:call).returns('I can do the thing')
+    }
+  }
+
+  context 'Run with defaults.' do
+    it { should compile }
+
+    it {
+      should contain_class('cassandra::firewall_ports').with({
+        'ssh_port' => 22
+      })
+    }
+  end
 
   context 'Ensure public_subnets can be bypassed.' do
     let :params do
