067d2b64887e54ebcd055d06b6498cade88ec045
dallinb <b.dalling@locp.co.uk>
2015-08-09 10:58:42 +0100

    New class to manage the DSC repository.

---
+++ manifests/dsc_repo.pp
@@ -0,0 +1,48 @@
+# == Class: cassandra::dsc_repo
+#
+# Please see the README for this module for full details of what this class
+# does as part of the module and how to use it.
+#
+class cassandra::dsc_repo {
+  case $::osfamily {
+    'RedHat': {
+      yumrepo { 'datastax':
+        ensure   => present,
+        descr    => 'DataStax Repo for Apache Cassandra',
+        baseurl  => 'http://rpm.datastax.com/community',
+        enabled  => 1,
+        gpgcheck => 0,
+      }
+    }
+    'Debian': {
+      include apt
+      include apt::update
+
+      apt::key {'datastaxkey':
+        id     => '7E41C00F85BFC1706C4FFFB3350200F2B999A372',
+        source => 'http://debian.datastax.com/debian/repo_key',
+        before => Apt::Source['datastax']
+      }
+
+      apt::source {'datastax':
+        location => 'http://debian.datastax.com/community',
+        comment  => 'DataStax Repo for Apache Cassandra',
+        release  => 'stable',
+        include  => {
+          'src' => false
+        },
+        notify   => Exec['update-cassandra-repos']
+      }
+
+      # Required to wrap apt_update
+      exec {'update-cassandra-repos':
+        refreshonly => true,
+        command     => '/bin/true',
+        require     => Exec['apt_update'],
+      }
+    }
+    default: {
+      warning("OS family ${::osfamily} not supported")
+    }
+  }
+}
---
+++ spec/classes/dsc_repo_spec.rb
@@ -0,0 +1,40 @@
+require 'spec_helper'
+describe 'cassandra::dsc_repo' do
+  let(:pre_condition) { [
+    'class apt () {}',
+    'class apt::update () {}',
+    'define apt::key ($id, $source) {}',
+    'define apt::source ($location, $comment, $release, $include) {}',
+  ] }
+
+  context 'Regardless of which OS' do
+    it { should compile }
+    it { should contain_class('cassandra::dsc_repo') }
+  end
+
+  context 'On a RedHat OS with defaults for all parameters' do
+    let :facts do
+      {
+        :osfamily => 'RedHat'
+      }
+    end
+
+    it { should contain_yumrepo('datastax') }
+  end
+
+  context 'On a Debian OS with defaults for all parameters' do
+    let :facts do
+      {
+        :osfamily => 'Debian',
+        :lsbdistid => 'Ubuntu',
+        :lsbdistrelease => '14.04'
+      }
+    end
+
+    it { should contain_class('apt') }
+    it { should contain_class('apt::update') }
+    it { is_expected.to contain_apt__key('datastaxkey') }
+    it { is_expected.to contain_apt__source('datastax') }
+    it { is_expected.to contain_exec('update-cassandra-repos') }
+  end
+end
