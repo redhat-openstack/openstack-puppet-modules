e26c9807084b388c16d7fe05ab14fb00d54e7454
dallinb <b.dalling@locp.co.uk>
2015-09-14 12:59:28 +0100

    Tougher checks for the cassandra::opscenter::pycrypto class (#106).

--- spec/classes/opscenter/pycyrpto_spec.rb
+++ spec/classes/opscenter/pycyrpto_spec.rb
@@ -8,10 +8,15 @@ describe 'cassandra::opscenter::pycrypto' do
     end
 
     it { should have_resource_count(4) }
-    it {should contain_class('cassandra::opscenter::pycrypto').with_package_name('pycrypto') }
-    it { should contain_class('cassandra::opscenter::pycrypto').with_ensure('present') }
-    it { should contain_class('cassandra::opscenter::pycrypto').with_manage_epel('false') }
-    it { should contain_class('cassandra::opscenter::pycrypto').with_provider('pip') }
+    it {
+      should contain_class('cassandra::opscenter::pycrypto').only_with({
+        'ensure'       => 'present',
+        'manage_epel'  => false,
+        'package_name' => 'pycrypto',
+        'provider'     => 'pip',
+        'reqd_pckgs'   => ['python-devel', 'python-pip'],
+      })
+    }
     it { should contain_package('pycrypto') }
     it { should contain_file('/usr/bin/pip-python') }
     it { should contain_package('python-devel') }
