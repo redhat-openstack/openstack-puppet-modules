4fc714c4e9efc6be46fafa67175718b161130b03
dallinb <b.dalling@locp.co.uk>
2015-09-15 19:31:24 +0100

    Rake lint was complaining (#110).

--- manifests/datastax_agent.pp
+++ manifests/datastax_agent.pp
@@ -31,7 +31,7 @@ class cassandra::datastax_agent (
   }
 
   service { $service_name:
-    ensure    => $service_ensure,
-    enable    => $service_enable,
+    ensure => $service_ensure,
+    enable => $service_enable,
   }
 }
