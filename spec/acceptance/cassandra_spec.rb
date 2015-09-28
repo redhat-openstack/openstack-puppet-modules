54d10e4306c5e6c328062f1249de70336ffcffd8
dallinb <b.dalling@locp.co.uk>
2015-09-28 17:38:53 +0100

    A new look acceptance test script (#115).

--- spec/acceptance/cassandra_spec.rb
+++ spec/acceptance/cassandra_spec.rb
@@ -1,89 +1,129 @@
 require 'spec_helper_acceptance'
 
 describe 'cassandra class' do
-  install_pp = <<-EOS
-    class { '::cassandra::datastax_repo': } ->
-    class { '::cassandra::java': } ->
+  pre_req_install_pp = <<-EOS
+    include '::cassandra::datastax_repo'
+    include '::cassandra::java'
+  EOS
+
+  describe 'Pre-requisits installation.' do
+    it 'should work with no errors' do
+      apply_manifest(pre_req_install_pp, :catch_failures => true)
+    end
+    it 'check code is idempotent' do
+      expect(apply_manifest(pre_req_install_pp, :catch_failures => true).exit_code).to be_zero
+    end
+  end
+
+  cassandra_install_pp = <<-EOS
     class { 'cassandra':
-      service_ensure              => 'stopped',
-      service_enable              => false,
       cassandra_9822              => true,
       commitlog_directory_mode    => '0770',
       data_file_directories_mode  => '0770',
       saved_caches_directory_mode => '0770'
-    } ->
-    class { '::cassandra::optutils': } ->
-    class { '::cassandra::datastax_agent':
-      service_ensure => 'stopped',
-      service_enable => false,
-    } ->
-    class { '::cassandra::opscenter::pycrypto':
-      manage_epel => true,
-    } ->
-    class { '::cassandra::opscenter':
-      service_ensure => 'stopped',
-      service_enable => false,
-    } ->
-    class { '::cassandra::firewall_ports': }
-
-    # A workaround for Issue79.
-    if $::osfamily == 'Debian' {
-      exec { '/bin/chown root:root /etc/apt/sources.list.d/datastax.list':
-        refreshonly => true,
-        subscribe   => Class[::cassandra::datastax_agent]
-      }
     }
   EOS
 
-  describe 'Initial install.' do
+  describe 'Cassandra installation.' do
     it 'should work with no errors' do
-      apply_manifest(install_pp, :catch_failures => true)
+      apply_manifest(cassandra_install_pp, :catch_failures => true)
+    end
+    it 'check code is idempotent' do
+      expect(apply_manifest(cassandra_install_pp, :catch_failures => true).exit_code).to be_zero
     end
   end
 
-  idempotent_pp = <<-EOS
-    class { '::cassandra::datastax_repo': } ->
-    class { '::cassandra::java': } ->
+  optutils_install_pp = <<-EOS
     class { 'cassandra':
-      service_ensure              => 'stopped',
-      service_enable              => false,
       cassandra_9822              => true,
       commitlog_directory_mode    => '0770',
       data_file_directories_mode  => '0770',
       saved_caches_directory_mode => '0770'
-    } ->
-    class { '::cassandra::optutils': } ->
-    class { '::cassandra::datastax_agent':
-      service_ensure => 'stopped',
-      service_enable => false,
-    } ->
-    class { '::cassandra::opscenter::pycrypto':
-      manage_epel => true,
-    } ->
-    class { '::cassandra::opscenter':
-      service_ensure => 'stopped',
-      service_enable => false,
     }
+    include '::cassandra::optutils'
   EOS
 
-  describe 'Idempotency test.' do
+  describe 'Cassandra optional utilities installation.' do
     it 'should work with no errors' do
-      expect(apply_manifest(idempotent_pp, :catch_failures => true).exit_code).to be_zero
+      apply_manifest(optutils_install_pp, :catch_failures => true)
+    end
+    it 'check code is idempotent' do
+      expect(apply_manifest(optutils_install_pp, :catch_failures => true).exit_code).to be_zero
     end
   end
 
-  #describe service('cassandra') do
-    #it { is_expected.to be_running }
-    #it { is_expected.to be_enabled }
-  #end
+  datastax_agent_install_pp = <<-EOS
+    class { 'cassandra':
+      cassandra_9822              => true,
+      commitlog_directory_mode    => '0770',
+      data_file_directories_mode  => '0770',
+      saved_caches_directory_mode => '0770'
+    }
+    include '::cassandra::datastax_agent'
+  EOS
 
-  #describe service('datastax-agent') do
-    #it { is_expected.to be_running }
-    #it { is_expected.to be_enabled }
-  #end
+  describe 'DataStax agent installation.' do
+    it 'should work with no errors' do
+      apply_manifest(datastax_agent_install_pp, :catch_failures => true)
+    end
+    it 'check code is idempotent' do
+      expect(apply_manifest(datastax_agent_install_pp, :catch_failures => true).exit_code).to be_zero
+    end
+  end
 
-  #describe service('opscenterd') do
-    #it { is_expected.to be_running }
-    #it { is_expected.to be_enabled }
-  #end
+  opscenter_install_pp = <<-EOS
+    class { '::cassandra::opscenter::pycrypto':
+      manage_epel => true,
+      before      => Class['::cassandra::opscenter']
+    }
+
+    include '::cassandra::opscenter'
+  EOS
+
+  describe 'OpsCenter installation.' do
+    it 'should work with no errors' do
+      apply_manifest(opscenter_install_pp, :catch_failures => true)
+    end
+    it 'check code is idempotent' do
+      expect(apply_manifest(opscenter_install_pp, :catch_failures => true).exit_code).to be_zero
+    end
+  end
+
+  firewall_config_pp = <<-EOS
+    class { 'cassandra':
+      cassandra_9822              => true,
+      commitlog_directory_mode    => '0770',
+      data_file_directories_mode  => '0770',
+      saved_caches_directory_mode => '0770'
+    }
+    include '::cassandra::optutils'
+    include '::cassandra::datastax_agent'
+    include '::cassandra::opscenter'
+    include '::cassandra::opscenter::pycrypto'
+    include '::cassandra::firewall_ports'
+  EOS
+
+  describe 'Firewall configuration.' do
+    it 'should work with no errors' do
+      apply_manifest(firewall_config_pp, :catch_failures => true)
+    end
+    it 'check code is idempotent' do
+      expect(apply_manifest(firewall_config_pp, :catch_failures => true).exit_code).to be_zero
+    end
+  end
+
+  describe service('cassandra') do
+    it { is_expected.to be_running }
+    it { is_expected.to be_enabled }
+  end
+
+  describe service('datastax-agent') do
+    it { is_expected.to be_running }
+    it { is_expected.to be_enabled }
+  end
+
+  describe service('opscenterd') do
+    it { is_expected.to be_running }
+    it { is_expected.to be_enabled }
+  end
 end
