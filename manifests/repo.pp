57440c5b4878ebfe483bb75ee5a63f98b75f4e8f
Eslam ElHusseiny <eslam.husseiny@gmail.com>
2015-11-23 10:02:35 +0000

    add support for cloudera cdh repo version 5

--- manifests/repo.pp
+++ manifests/repo.pp
@@ -5,6 +5,7 @@
 
 class zookeeper::repo(
   $source = undef,
+  $cdhver = undef,
   $ensure = present,
 ) {
 
@@ -14,41 +15,69 @@ class zookeeper::repo(
         case $source {
           undef: {} #nothing to do
           'cloudera': {
-            $cdhver = '4'
-            $osrel = $::operatingsystemmajrelease
-
-            case $::hardwaremodel {
-              'i386', 'x86_64': {
-                $repourl = "http://archive.cloudera.com/cdh${cdhver}/redhat/6/${::hardwaremodel}/cdh/cloudera-cdh${cdhver}.repo"
-              }
-              default: {
-                fail("Yum repository '${source}' is not supported for architecture ${::hardwaremodel}")
-              }
+            if $cdhver == undef {
+              fail('Cloudera repo is required, but no CDH version is provider.')
             }
-            case $osrel {
-              '6', '7': {
-                # parameter ensure is not supported before Puppet 3.5
-                if versioncmp($::puppetversion, '3.5.0') >= 0 {
-                  yumrepo { "cloudera-cdh${cdhver}":
-                    ensure   => $ensure,
-                    descr    => "Cloudera's Distribution for Hadoop, Version ${cdhver}",
-                    baseurl  => "http://archive.cloudera.com/cdh${cdhver}/redhat/6/${::hardwaremodel}/cdh/${cdhver}/",
-                    gpgkey   => "http://archive.cloudera.com/cdh${cdhver}/redhat/6/${::hardwaremodel}/cdh/RPM-GPG-KEY-cloudera",
-                    gpgcheck => 1
+            $osrel = $::operatingsystemmajrelease
+            case $cdhver {
+              '4': {
+                case $::hardwaremodel {
+                  'i386', 'x86_64': {
+                    case $osrel {
+                      '6', '7': {
+                        $release = '6'
+                      }
+                      default: {
+                        fail("Yum repository '${source}' is not supported for redhat version ${osrel}")
+                      }
+                    }
                   }
-                } else {
-                  yumrepo { "cloudera-cdh${cdhver}":
-                    descr    => "Cloudera's Distribution for Hadoop, Version ${cdhver}",
-                    baseurl  => "http://archive.cloudera.com/cdh${cdhver}/redhat/6/${::hardwaremodel}/cdh/${cdhver}/",
-                    gpgkey   => "http://archive.cloudera.com/cdh${cdhver}/redhat/6/${::hardwaremodel}/cdh/RPM-GPG-KEY-cloudera",
-                    gpgcheck => 1
+                  default: {
+                    fail("Yum repository '${source}' is not supported for architecture ${::hardwaremodel}")
+                  }
+                }
+              }
+              '5': {
+                case $::hardwaremodel {
+                  'x86_64': {
+                    case $osrel {
+                      '6', '7': {
+                        $release = $osrel
+                      }
+                      default: {
+                        fail("Yum repository '${source}' is not supported for redhat version ${osrel}")
+                      }
+                    }
+                  }
+                  default: {
+                    fail("Yum repository '${source}' is not supported for architecture ${::hardwaremodel}")
                   }
                 }
               }
               default: {
-                fail("Yum repository '${source}' is not supported for redhat version ${osrel}")
+                fail("CDH version '${cdhver}' is not supported.")
               }
             }
+            # parameter ensure is not supported before Puppet 3.5
+            if versioncmp($::puppetversion, '3.5.0') >= 0 {
+              yumrepo { "cloudera-cdh${cdhver}":
+                ensure   => $ensure,
+                descr    => "Cloudera's Distribution for Hadoop, Version ${cdhver}",
+                baseurl  => "http://archive.cloudera.com/cdh${cdhver}/redhat/${release}/${::hardwaremodel}/cdh/${cdhver}/",
+                gpgkey   => "http://archive.cloudera.com/cdh${cdhver}/redhat/${release}/${::hardwaremodel}/cdh/RPM-GPG-KEY-cloudera",
+                gpgcheck => 1
+              }
+            } else {
+              yumrepo { "cloudera-cdh${cdhver}":
+                descr    => "Cloudera's Distribution for Hadoop, Version ${cdhver}",
+                baseurl  => "http://archive.cloudera.com/cdh${cdhver}/redhat/${osrel}/${::hardwaremodel}/cdh/${cdhver}/",
+                gpgkey   => "http://archive.cloudera.com/cdh${cdhver}/redhat/${osrel}/${::hardwaremodel}/cdh/RPM-GPG-KEY-cloudera",
+                gpgcheck => 1
+              }
+            }
+          }
+          default: {
+            fail("\"${module_name}\" provides no repository information for yum repository \"${source}\"")
           }
         }
       }
